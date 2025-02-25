d3b8f7d696ef94679ade9c430989ef16dfb3e95d
Levirroh <johann.gr14@gmail.com>
2025-02-25 13:32:12 -0300

    feat: js used to change html

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("teste");
+var nome = prompt("Qual o seu nome?");
+var sobrenome = prompt("Qual o seu sobrenome?")
+console.log("Olá "+ nome + ' ' + sobrenome, "!");
+console.log("Olá", nome, sobrenome, "!");
+var nomeCompleto = nome + " " + sobrenome;
+
+
+document.write("<h1> Bem vindo ao projeto "+ nomeCompleto + "!</h1>");
+document.write("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIymDFMjBfIKJqVOf9CJjtOOWESd211Ha5jA&s' alt='Foto de um gatinho fofinho'>");
