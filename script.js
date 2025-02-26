9982e0563976acd0410df6abedc5b55629bfd393
Levirroh <johann.gr14@gmail.com>
2025-02-26 09:09:21 -0300

    feat: created 2 new functions

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ function sair(){
     alert("Até mais!");
     area.innerHTML = "Você saiu";
 }
+
+
+function mediaAluno (nota1, nota2){ // usar o próprio inspecionar informando a mediaAluno(notas)
+    var media = (nota1 + nota2) / 2;
+    if (media >= 7 ){
+       console.log("Sua média foi "+ media + " você passou!");
+    } else {
+        console.log("Sua média foi "+ media + " você não passou...");
+    }
+}
+
+function aluno (nome, curso){
+    var mensagem = "Seja bem vindo "+ nome + ", ao curso de " + curso;
+    console.log(mensagem);
+}
