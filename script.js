5b5e402e1280d04ed6473887bccbf5ed6b9fefca
Levirroh <johann.gr14@gmail.com>
2025-02-26 09:01:32 -0300

    feat: exit button created

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function entrar(){
         area.innerHTML = "Clique no botão para acessar";
         alert("Oops, algo deu errado!");
     } else{
-        area.innerHTML = "Bem vindo " + nome;
+        area.innerHTML = "Bem vindo " + nome + "    ";
+
+        let botaoSair = document.createElement("button");
+        botaoSair.innerText = "Sair da conta";
+        botaoSair.onclick = sair;
+
+        area.appendChild(botaoSair);
+
     }
+}
+
+function sair(){
+    alert("Até mais!");
+    area.innerHTML = "Você saiu";
 }
