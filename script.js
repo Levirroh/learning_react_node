2f0136f676650aa2bbcc6c6bdf20747d0fa4a0c9
Levirroh <johann.gr14@gmail.com>
2025-02-26 08:56:54 -0300

    update: remake script

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var nome = prompt("Qual o seu nome?");
-var sobrenome = prompt("Qual o seu sobrenome?")
-console.log("Olá "+ nome + ' ' + sobrenome, "!");
-console.log("Olá", nome, sobrenome, "!");
-var nomeCompleto = nome + " " + sobrenome;
+var area = document.getElementById("area");
 
-
-document.write("<h1> Bem vindo ao projeto "+ nomeCompleto + "!</h1>");
-document.write("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIymDFMjBfIKJqVOf9CJjtOOWESd211Ha5jA&s' alt='Foto de um gatinho fofinho'>");
+function entrar(){
+    var nome = prompt("Digite o seu nome");
+    if (nome === '' || nome === null){
+        area.innerHTML = "Clique no botão para acessar";
+        alert("Oops, algo deu errado!");
+    } else{
+        area.innerHTML = "Bem vindo " + nome;
+    }
+}
