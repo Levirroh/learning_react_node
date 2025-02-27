6234b42d5a542b17fc9d832a520cbb5da9d2f8d3
Levirroh <johann.gr14@gmail.com>
2025-02-27 10:31:26 -0300

    update: result to 2 decimal max

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ function calcular(event){  // normalmente o event faz um refresh na pagina
  
     
     if (imc < 17){
-        resultado.innerHTML = "<br>Seu resultado foi: "+imc+"<br> Cuidado, você está muito abaixo do peso.";
+        resultado.innerHTML = "<br>Seu resultado foi: "+imc.toFixed(2)+"<br> Cuidado, você está muito abaixo do peso.";
     } else if (imc >= 17 && imc <= 18.49){
-        resultado.innerHTML = "<br>Seu resultado foi: "+imc+"<br>Você está abaixo do peso.";
+        resultado.innerHTML = "<br>Seu resultado foi: "+imc.toFixed(2)+"<br>Você está abaixo do peso.";
     } else if (imc >= 18.5 && imc <= 24.99){
-        resultado.innerHTML = "<br>Seu resultado foi: "+imc+"<br>Você está dentro do peso esperado.";
+        resultado.innerHTML = "<br>Seu resultado foi: "+imc.toFixed(2)+"<br>Você está dentro do peso esperado.";
     }else if (imc >= 25 && imc <= 29.99){
-        resultado.innerHTML = "<br>Seu resultado foi: "+imc+"<br>Você está acima do peso esperado.";
+        resultado.innerHTML = "<br>Seu resultado foi: "+imc.toFixed(2)+"<br>Você está acima do peso esperado.";
     }else {
-        resultado.innerHTML = "<br>Algo deu errado.";
+        resultado.innerHTML = "<br>Seu resultado foi: "+imc.toFixed(2)+"<br>Cuidado, você está na zona de obesidade.";
     }
     
 
