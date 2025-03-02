d628afdbf8d6784077fc584f1f3fb7e664afd7b0
Levirroh <johann.gr14@gmail.com>
2025-03-02 10:12:31 -0300

    feat: console shows when timeout happens

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
 // // setInterval
 
-// function acao(){
-//     document.write("Executando... <br>");
-// };
+function acao(){
+    document.write("Executando... <br>");
+};
 
 // setInterval(acao, 1000);
 
 
-var timer = setInterval(() => {
-    document.write("Executando!!! <br>");
-}, 1000);
+// var timer = setInterval(() => {
+//     document.write("Executando!!! <br>");
+// }, 1000);
+
+
+// passing clearInterval(timer) in the terminal you can stop de timer
+
+setTimeout(() =>{
+    console.log("EXECUTOU O TIMEOUT")
+}, 3000);
