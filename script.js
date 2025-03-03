d647aea007d1c8b52e88608029e8234f68f6e8a3
Levirroh <johann.gr14@gmail.com>
2025-03-03 11:58:57 -0300

    feat: spread operator on an object that changes on an function

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-// // SPREAD OPERATOR
+// // // SPREAD OPERATOR
 
-// let primeiros = [1, 2, 3];
+// // let primeiros = [1, 2, 3];
 
-// let numeros = [...primeiros, 4, 5, 10]; // adiciona a array primeiros dentro da array numeros sem ser uma array dentro de uma array;
-// // sendo 1, 2, 3, 4, 5, 10
-
-
-// console.log(numeros);
+// // let numeros = [...primeiros, 4, 5, 10]; // adiciona a array primeiros dentro da array numeros sem ser uma array dentro de uma array;
+// // // sendo 1, 2, 3, 4, 5, 10
 
 
+// // console.log(numeros);
 
-let pessoa ={
-    nome: "Matheus",
-    idade: 45,
-    cargo: "RH"
-};
 
-let novaPessoa = {
-    ...pessoa,
-    status: "ATIVO",
-    cidade: "Campo Grande / MS",
-    telefone: "123123123"
-};
 
-console.log(novaPessoa);
+// let pessoa ={
+//     nome: "Matheus",
+//     idade: 45,
+//     cargo: "RH"
+// };
+
+// let novaPessoa = {
+//     ...pessoa,
+//     status: "ATIVO",
+//     cidade: "Campo Grande / MS",
+//     telefone: "123123123"
+// };
+
+// console.log(novaPessoa);
+
+
+function novoUsuario(info){
+    let data = {
+        ...info,
+        status: "Ativo",
+        iniciando: "20/03/2023",
+        codigo: "123123"
+    };
+    console.log(data);
+}
+
+novoUsuario({nome: "José", sobrenome: "Silva", cargo: "DEV"})
