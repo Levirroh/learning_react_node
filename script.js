7f6a3fa0433e28b42266669154f2249027dccfe5
Levirroh <johann.gr14@gmail.com>
2025-03-03 12:12:32 -0300

    feat: numbers drawer with rest operator

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-// // // SPREAD OPERATOR
+// // REST OPERATOR
 
-// // let primeiros = [1, 2, 3];
+// function convidados(...nome){
+//     console.log("SEJAM BEM VINDOS")
+//     console.log(nome)
+// }
 
-// // let numeros = [...primeiros, 4, 5, 10]; // adiciona a array primeiros dentro da array numeros sem ser uma array dentro de uma array;
-// // // sendo 1, 2, 3, 4, 5, 10
-
-
-// // console.log(numeros);
-
+// convidados("Matheus", "Maria", "Lucas")
 
 
-// let pessoa ={
-//     nome: "Matheus",
-//     idade: 45,
-//     cargo: "RH"
-// };
-
-// let novaPessoa = {
-//     ...pessoa,
-//     status: "ATIVO",
-//     cidade: "Campo Grande / MS",
-//     telefone: "123123123"
-// };
-
-// console.log(novaPessoa);
 
 
-function novoUsuario(info){
-    let data = {
-        ...info,
-        status: "Ativo",
-        iniciando: "20/03/2023",
-        codigo: "123123"
-    };
-    console.log(data);
+function sorteador(...numeros){
+    console.log(numeros)
+
+    const numeroAleatorio = Math.floor(Math.random() * numeros.length);
+    console.log(numeros[numeroAleatorio])
 }
 
-novoUsuario({nome: "José", sobrenome: "Silva", cargo: "DEV"})
+sorteador(1, 2, 3, 4, 5, 6)
