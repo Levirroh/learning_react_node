40c308db931a61b8b9dd35c206e296f133d39e31
Levirroh <johann.gr14@gmail.com>
2025-03-03 11:55:16 -0300

    feat: spread operator on objects

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// SPREAD OPERATOR
+// // SPREAD OPERATOR
 
-let primeiros = [1, 2, 3];
+// let primeiros = [1, 2, 3];
 
-let numeros = [...primeiros, 4, 5, 10]; // adiciona a array primeiros dentro da array numeros sem ser uma array dentro de uma array;
-// sendo 1, 2, 3, 4, 5, 10
+// let numeros = [...primeiros, 4, 5, 10]; // adiciona a array primeiros dentro da array numeros sem ser uma array dentro de uma array;
+// // sendo 1, 2, 3, 4, 5, 10
 
 
-console.log(numeros);
+// console.log(numeros);
 
+
+
+let pessoa ={
+    nome: "Matheus",
+    idade: 45,
+    cargo: "RH"
+};
+
+let novaPessoa = {
+    ...pessoa,
+    status: "ATIVO",
+    cidade: "Campo Grande / MS",
+    telefone: "123123123"
+};
+
+console.log(novaPessoa);
