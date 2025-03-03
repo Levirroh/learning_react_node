544045e85c4cde76c27b699d719abaa7b69bb445
Levirroh <johann.gr14@gmail.com>
2025-03-03 11:52:05 -0300

    feat: spread operator on arrays

---
+++ script.js
@@ -0,0 +1,10 @@
+// SPREAD OPERATOR
+
+let primeiros = [1, 2, 3];
+
+let numeros = [...primeiros, 4, 5, 10]; // adiciona a array primeiros dentro da array numeros sem ser uma array dentro de uma array;
+// sendo 1, 2, 3, 4, 5, 10
+
+
+console.log(numeros);
+
