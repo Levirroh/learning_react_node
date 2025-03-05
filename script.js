0b27437036072fe8ea7cf04560ac5247c97e573b
Levirroh <johann.gr14@gmail.com>
2025-03-05 09:36:53 -0300

    feat: filter js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // FIND
 
-let listagem = [5, 3, "Jose", 2, "Matheus"];
+// let listagem = [5, 3, "Jose", 2, "Matheus"];
 
-let busca = listagem.find(() => {
-    if (item === "Jose"){
-        return console.log("ITEM ENCONTRADO COM SUCESSO")
-    }
+// let busca = listagem.find(() => {
+//     if (item === "Jose"){
+//         return console.log("ITEM ENCONTRADO COM SUCESSO")
+//     }
+// })
+
+// console.log(busca);
+
+
+
+// filter
+
+let palavras = ["Matheus", "Ana", "Jose", "Ricardo Silva", "Sujeito programador"];
+
+
+
+let resultado = palavras.filter((item) => {
+    return item.length >= 4;
 })
 
-console.log(busca);
-
-
+console.log(resultado);
