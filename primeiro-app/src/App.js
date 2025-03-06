26f872f7997e5b0c2895be74e78e37be4a4dd8eb
Levirroh <johann.gr14@gmail.com>
2025-03-06 13:07:44 -0300

    update: app js

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,17 +1,32 @@
 import { useState } from 'react';
 
 function App() {
-  const [aluno, setAluno] = useState('Sujeito Programador');
-
-  function handleChangeName(nome){
-    setAluno(nome);
-  }
+  const [nome,setNome] = useState('');
+  const [email,setEmail] = useState('');
+  const [idade,setIdade] = useState('');
 
   return (
     <div>
-      <h1>Componente App</h1> <br/>
-      <h2>Olá: { aluno }</h2>
-      <button onClick={() => handleChangeName('Lucas Oliveira')}>Mudar Nome</button>
+      <h1>Cadastrando Usuário</h1>
+      <form>
+        <label>Nome:</label><br/>
+        <input placeholder='Digite seu nome' value={nome} onChange={ (e) => setNome(e.target.value)}/>
+        <br/>
+        <label>Email:</label><br/>
+        <input placeholder='Digite seu email' value={email} onChange={ (e) => setEmail(e.target.value)}/>
+        <br/>
+        <label>Idade:</label><br/>
+        <input placeholder='Digite seu idade' value={idade} onChange={ (e) => setIdade(e.target.value)}/>
+        <br/>
+        <button type='submit'>Registrar</button>
+      </form>
+      <br/>
+      <br/>
+      <div>
+        <p>Bem-vindo: Matheus</p><br/>
+        <p>idade: 25</p><br/>
+        <p>Email: teste@teste.com</p><br/>
+      </div>
     </div>
   );
 }
