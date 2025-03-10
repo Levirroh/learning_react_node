723985b2bdb59fd4a3eb39a99b7d327267c47572
Levirroh <johann.gr14@gmail.com>
2025-03-09 23:04:32 -0300

    feat: recreating project

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,51 +1,9 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [input, setInput] = useState('')
-  const [tasks,setTasks] = useState([
-    'Pagar conta de luz',
-    'Estudar React JS'
-  ]);
-
-  useEffect(() => {
-   const tasksStorage = localStorage.getItem('@tasks');
-
-   if(tasksStorage){
-    setTasks(JSON.parse(tasksStorage));
-   } 
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('@tasks', JSON.stringify(tasks));
-  }, [tasks]);
-
-  function handleRegister(e){
-    e.preventDefault();
-    
-    // if (input.trim() === ""){ // .trim() -> remove espaços em branco, ou seja, se digitar um monte de espaço ainda não é aceito
-    //   alert("Digite um valor válido para a tarefa!")
-    // } else{
-      setTasks([...tasks, input]); // pega todas as que já tinham na array e soma as do useState input que está sendo lido ao enviar o formulário
-    // }
-    setInput(''); // limpa campo digitado
-  } 
-
   return (
     <div>
-      <h1>Cadastrando Usuário</h1>
-      <form onSubmit={handleRegister}>
-        <label>Nome da tarefa:</label><br/>
-        <input placeholder='Digite uma tarefa' value={input} onChange={ (e) => setInput(e.target.value)}/>
-       
-        <button type='submit'>Adicionar</button>
-      </form>
-      <br/>
-      <br/>
-      <ul>
-        {tasks.map( task => (
-          <li key={task}>{task}</li>
-        ))}
-      </ul>
+      
     </div>
   );
 }
