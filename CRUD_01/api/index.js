b594abfc765dfebb0da29373ee2ca2d8f97c1042
Levirroh <johann.gr14@gmail.com>
2025-03-11 11:35:31 -0300

    feat: js arquives

--- CRUD_01/api/index.js
+++ CRUD_01/api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import userRoutes from "./routes/users.js"
 import cors from "cors";
 
 
@@ -6,5 +7,8 @@ const app = express();
 
 app.use(express.json())
 app.use(cors());
+
+
+app.use("/", userRoutes)
 
 app.listen(8800)
---
+++ CRUD_01/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './styles/global.js';
+import { toast, TastoContainer } from "react-toastify";
+import "react-toastify/dist/ReactTo"
+
+const Container = StyleSheet.div`
+  width: 100%;
+`
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>asodimasd</p>
+    </div>
+  );
+}
+
+export default App;
