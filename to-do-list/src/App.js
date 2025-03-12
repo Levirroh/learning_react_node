406e85a136c6ce5bf1fe8c2a0f610b5000684c65
Levirroh <johann.gr14@gmail.com>
2025-03-12 16:16:18 -0300

    feat: setting up new node / react environment

---
+++ to-do-list/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import './index.css';
+
+function App() {
+
+  return (
+    <div className="App">
+     <p>Teste</p>
+    </div>
+  );
+}
+
+export default App;
