275bed2d71ad81229fdc4ae3746ecefdb9beb1fe
Levirroh <johann.gr14@gmail.com>
2025-03-12 19:11:12 -0300

    minor changes

--- to-do-list/src/components/Connection.js
+++ to-do-list/src/components/Connection.js
@@ -6,11 +6,6 @@ function Test() {
             <tr>
                 <th>Nome</th>
             </tr>
-            {users.map((item, i) => (
-                <tr key={i}>
-                    <td width="30%">{item.name_user}</td>
-                </tr>
-            ))}
         </table>
 
     );
