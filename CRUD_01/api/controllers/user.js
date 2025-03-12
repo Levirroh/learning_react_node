8078190a820e414b2cd38f541d1d6f347c614bac
Levirroh <johann.gr14@gmail.com>
2025-03-12 11:38:57 -0300

    feat: addUser & updateUser

--- CRUD_01/api/controllers/user.js
+++ CRUD_01/api/controllers/user.js
@@ -8,4 +8,48 @@ export const getUsers = (_, res) => {
 
         return res.status(200).json(data)
     })
+};
+
+export const addUser = (req, res) => {
+    const q = "INSERT INTO users(`name_user`, `email_user`, `phone_user`, `nasc_user`) VALUES (?)"
+
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.phone,
+        req.body.date,
+    ];
+
+    connection.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso");
+    })
 }
+
+export const updateUser = (req, res) => {
+    const q = "UPDATE users SET `name_user` = ?, `email_user` = ? `phone_user` = ?, `nasc_user` = ? WHERE `id_user` = ?"
+
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.phone,
+        req.body.date,
+    ];
+
+    connection.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário atualizado com sucesso");
+    })
+}
+
+export const deleteUser = (req, res) => {
+    const q = "DELETE FROM users WHERE `id_user` = ?";
+  
+    connection.query(q, [req.params.id], (err) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json("Usuário deletado com sucesso.");
+    });
+  };
