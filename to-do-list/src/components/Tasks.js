924fa92b85b7c01fc2adb8479970a7face941e4d
Levirroh <johann.gr14@gmail.com>
2025-03-13 10:05:24 -0300

    feat: tasks filtering by status

---
+++ to-do-list/src/components/Tasks.js
@@ -0,0 +1,33 @@
+function Tasks({tasks}) {
+    return (
+        <section>
+            <div id="ToDo">
+            <p>TO DO</p>
+            <ul>
+                {tasks.filter(task => task.status_task === "ToDo").map((task, index) => (
+                <li key={index}>{task.id_task} - {task.status_task}</li> 
+                ))}
+            </ul>
+            </div>
+            <div id="Doing">
+            <p>Doing</p>
+            <ul>
+                {tasks.filter(task => task.status_task === "Doing").map((task, index) => (
+                <li key={index}>{task.id_task} - {task.status_task}</li> 
+                ))}
+            </ul>
+            </div>
+            <div id="Done">
+            <p>Done</p>
+            <ul>
+                {tasks.filter(task => task.status_task === "Done").map((task, index) => (
+                <li key={index}>{task.id_task} - {task.status_task}</li> 
+                ))}
+            </ul>
+            </div>
+        </section>
+    );
+}
+
+
+export default Tasks;
