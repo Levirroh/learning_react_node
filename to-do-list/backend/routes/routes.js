f6a1ea1c2f5e3ecd476a53b1cd9d73f39a593887
Levirroh <johann.gr14@gmail.com>
2025-03-13 09:47:34 -0300

    feat: new imports from tasks.ks

--- to-do-list/backend/routes/routes.js
+++ to-do-list/backend/routes/routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers } from "../controllers/user.js"
+import { getUsers} from "../controllers/user.js";
+import {getTasks ,getDoneTasks, getDoingTasks, getToDoTasks} from "../controllers/tasks.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
+router.get("/", getTasks, getUsers);
 
 
 export default router;
