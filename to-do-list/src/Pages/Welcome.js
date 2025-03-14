57ad66b0267a220a0bfbefada18fa4b00c069bbb
Levirroh <johann.gr14@gmail.com>
2025-03-14 20:56:44 -0300

    feat: welcome page basics

--- to-do-list/src/Pages/Welcome.js
+++ to-do-list/src/Pages/Welcome.js
@@ -2,7 +2,17 @@ import React, { useEffect, useState } from 'react';
 
 function Welcome() {
     return(
-        <p>Welcome</p>
+        <section>
+            <div>
+                <h1>Bem-vindo</h1>
+                <p>Este software foi feito com Node.js & React, aqui você pode gerenciar tarefas e usuários, além disso, possui um banco de dados completo, aproveite!</p>
+            </div>
+            <div>
+                <a href="http://localhost:3000/login"><button>Entre já!</button></a>
+                <br/>
+                <a href="http://localhost:3000/register"><button>Cadastre-se já!</button></a>
+            </div>
+        </section>
     );
 }
 
