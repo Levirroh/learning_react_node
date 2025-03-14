0b3532f6d524c5b3be3abb600e740d4838a2182a
Levirroh <johann.gr14@gmail.com>
2025-03-14 20:47:55 -0300

    feat: imported new pages & added routes to them

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,14 +1,25 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Pages/App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./Pages/App";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Welcome from "./Pages/Welcome";
+
+import "./index.css";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}></Route>        
+        <Route path="/" element={<Welcome/>}></Route>
+        <Route path="/login" element={<Login/>}></Route> 
+        <Route path="/register" element={<Register/>}></Route> 
+        <Route path="/menu" element={<App/>}></Route>         
       </Routes>
     </BrowserRouter>
   </StrictMode>
