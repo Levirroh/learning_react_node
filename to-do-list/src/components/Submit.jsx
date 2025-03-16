74d6cef7e380ddcfe04b9c2b36b909fe635e7182
Levirroh <johann.gr14@gmail.com>
2025-03-16 20:47:13 -0300

    feat: new component Submit to padronize and optimize comde

---
+++ to-do-list/src/components/Submit.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+function Input({text}) {
+    return(
+        <button className="bg-gray-200 w-30 p-1 border-1 rounded-2xl shadow-2xl shadow-black cursor-pointer hover:bg-blue-700 hover:text-white hover:border-black transition-all" type="submit">
+            {text}
+        </button>
+    );
+}
+
+export default Input;
