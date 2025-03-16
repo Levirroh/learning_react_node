8fc1ac0a006191ebbc354cbfff126536c1cb8dba
Levirroh <johann.gr14@gmail.com>
2025-03-16 20:47:51 -0300

    feat: avanced Register page

--- to-do-list/src/Pages/Register.jsx
+++ to-do-list/src/Pages/Register.jsx
@@ -1,9 +1,23 @@
 import React from "react"; 
+import Submit from "../components/Submit"
+import Input from "../components/Input"
 
 function Register() {
     return(
-        <p>Register</p>
+        <section className="flex flex-col items-center justify-center bg-gray-300 h-dvh w-dvw">
+            <form className="flex flex-col bg-blue-300 border-1 p-3 gap-5 rounded-2xl">
+                <Input label="Nome:" placeholder="..."/>
+                <Input label="Email:" placeholder="email@email.com"/>
+                <Input label="Senha:" placeholder="..."/>
+                <Input label="Confirme sua senha:" placeholder="..."/>
+                <div className="flex items-center justify-center">
+                    <a href="http://localhost:5173/menu">
+                    <Submit text="Cadastre-se"/>
+                    </a>
+                </div>
+            </form>
+        </section>
     );
-}
+};
 
 export default Register;
