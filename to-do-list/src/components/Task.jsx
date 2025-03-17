75b11d5075a881fa2c3cd31e8732428f2ae57b76
Levirroh <johann.gr14@gmail.com>
2025-03-17 13:02:18 -0300

    update: status affects task color

--- to-do-list/src/components/Task.jsx
+++ to-do-list/src/components/Task.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 function Task({id, user, title, description, subject, date, status}){
     return(
-        <li>
-            {id} - {status} <br/>
+        <div className={`flex justify-between items-center flex-col border-1 p-2 rounded-2xl ${status} mt-3 text-left`}>
+            id: {id} <br/>
             {user} - {title} <br/>
             {description} - {subject} <br/>
             {date}<br/>
-        </li> 
+        </div> 
 );
 }
 
