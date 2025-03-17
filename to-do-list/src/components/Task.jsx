15a9f585042e6878dedb6b1d7024a06daf7611dc
Levirroh <johann.gr14@gmail.com>
2025-03-17 13:09:55 -0300

    update: text on left

--- to-do-list/src/components/Task.jsx
+++ to-do-list/src/components/Task.jsx
@@ -2,10 +2,12 @@ import React from "react"
 
 function Task({id, user, title, description, subject, date, status}){
     return(
-        <div className={`flex justify-between items-center flex-col border-1 p-2 rounded-2xl ${status} mt-3 text-left`}>
+        <div className={`flex flex-col border-1 p-2 rounded-2xl ${status} mt-3 text-left`}>
             id: {id} <br/>
-            {user} - {title} <br/>
-            {description} - {subject} <br/>
+            user: {user}<br/>
+            title: {title} <br/>
+            description: {description}<br/>
+            subject: {subject} <br/>
             {date}<br/>
         </div> 
 );
