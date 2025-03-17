622c93b46f0c15d313a0e556f63286d3bb07edd9
Levirroh <johann.gr14@gmail.com>
2025-03-16 21:08:24 -0300

    feat: Tasks ask for Task

--- to-do-list/src/components/Tasks.jsx
+++ to-do-list/src/components/Tasks.jsx
@@ -1,31 +1,54 @@
 import React from "react";
+import Task from "./Task";
+
 
 function Tasks({tasks}) {
     return (
-        <section>
+        <section className="flex h-svh w-svw justify-evenly"> 
             <div id="ToDo">
-            <p>TO DO</p>
-            <ul>
-                {tasks.filter(task => task.status_task === "ToDo").map((task, index) => (
-                <li key={index}>{task.id_task} - {task.status_task}</li> 
-                ))}
-            </ul>
+                <p>TO DO</p>
+                <ul className="bg-blue-200">
+                    {tasks.filter(task => task.status_task === "ToDo").map((task, index) => (
+                        <Task key={index} 
+                        id={task.id_task} 
+                        user={task.user_task} 
+                        title={task.title_task} 
+                        description={task.description_task} 
+                        subject={task.subject_task} 
+                        date={task.date_task} 
+                        status={task.status_task}/>
+                    ))}
+                </ul>
             </div>
             <div id="Doing">
-            <p>Doing</p>
-            <ul>
-                {tasks.filter(task => task.status_task === "Doing").map((task, index) => (
-                <li key={index}>{task.id_task} - {task.status_task}</li> 
-                ))}
-            </ul>
+                <p>Doing</p>
+                <ul className="bg-yellow-200">
+                    {tasks.filter(task => task.status_task === "Doing").map((task, index) => (
+                        <Task key={index} 
+                        id={task.id_task} 
+                        user={task.user_task} 
+                        title={task.title_task} 
+                        description={task.description_task} 
+                        subject={task.subject_task} 
+                        date={task.date_task} 
+                        status={task.status_task}/>
+                    ))}
+                </ul>
             </div>
             <div id="Done">
-            <p>Done</p>
-            <ul>
-                {tasks.filter(task => task.status_task === "Done").map((task, index) => (
-                <li key={index}>{task.id_task} - {task.status_task}</li> 
-                ))}
-            </ul>
+                <p>Done</p>
+                <ul className="bg-green-200">
+                    {tasks.filter(task => task.status_task === "Done").map((task, index) => (
+                        <Task key={index} 
+                        id={task.id_task} 
+                        user={task.user_task} 
+                        title={task.title_task} 
+                        description={task.description_task} 
+                        subject={task.subject_task} 
+                        date={task.date_task} 
+                        status={task.status_task}/> 
+                    ))}
+                </ul>
             </div>
         </section>
     );
