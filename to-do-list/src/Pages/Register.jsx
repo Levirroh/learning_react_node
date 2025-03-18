65d867a32902258b496f8d3820f3810f8d8e8605
Levirroh <johann.gr14@gmail.com>
2025-03-18 10:15:32 -0300

    fix: sending data to router to database

--- to-do-list/src/Pages/Register.jsx
+++ to-do-list/src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React, { useEffect, useState } from "react"; 
 import { useNavigate } from "react-router-dom";
 import Submit from "../components/Submit"
 import Input from "../components/Input"
@@ -7,27 +7,47 @@ import Input from "../components/Input"
 function Register() {
     const navigate = useNavigate();
     
-    function createUser(event){
+    async function createUser(event){
         event.preventDefault(); 
         const formData = new FormData(event.target);
-        const nome = formData.get("nome");
-        const email = formData.get("email");
-        const senha = formData.get("senha");
+        const name_user = formData.get("name_user");
+        const email_user = formData.get("email_user");
+        const function_user = formData.get("function_user");
+        const password_user = formData.get("password_user");
         const confirma_senha = formData.get("confirma_senha");
 
-        if (senha === confirma_senha) {
-            alert("Usuário cadastrado com sucesso!");
-        } else {
+        if (password_user !== confirma_senha) {
             alert("As senhas não coincidem!");
+            return;
         }
-    }
+
+        try {
+
+            const response = await fetch("http://localhost:8800/createUser",  {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name_user, email_user, function_user, password_user })
+        });
+        
+            if(!response.ok){
+                throw new Error('Error to create user');
+            }
+    
+        } catch (e) {
+            console.log('Error to create user: ', e);
+        }
+        navigate("/menu")
+    }; 
 
     return(
         <section className="flex flex-col items-center justify-center bg-gray-300 h-dvh w-dvw">
             <form className="flex flex-col bg-blue-300 border-1 p-3 gap-5 rounded-2xl" onSubmit={createUser}>
-                <Input id="name" name="nome" label="Nome:" placeholder="..."/>
-                <Input id="email" name="email" label="Email:" placeholder="email@email.com"/>
-                <Input id="senha" name="senha" label="Senha:" placeholder="..."/>
+                <Input id="name_user" name="name_user" label="Nome:" placeholder="..."/>
+                <Input id="email_user" name="email_user" label="Email:" placeholder="email@email.com"/>
+                <Input id="function_user" name="function_user" label="Função:" placeholder="..."/>
+                <Input id="password_user" name="password_user" label="Senha:" placeholder="..."/>
                 <Input id="confirma_senha" name="confirma_senha" label="Confirme sua senha:" placeholder="..."/>
                 <div className="flex items-center justify-center">
                     <a href="http://localhost:5173/menu">
