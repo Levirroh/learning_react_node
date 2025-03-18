b390129fab28ba9f6c18c1aed7a90564bed35698
Levirroh <johann.gr14@gmail.com>
2025-03-18 10:42:18 -0300

    feat: App gets user values on database showing its name on menu

--- to-do-list/src/Pages/App.jsx
+++ to-do-list/src/Pages/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 import Tasks from "../components/Tasks";
 import Header from "../components/Header";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+        setUser(JSON.parse(storedUser));
+    } else {
+        navigate("/login");
+    }
+  }, []);
 
   async function getTasks() {
 
@@ -19,24 +30,36 @@ function App() {
     });
 
       if(!response.ok){
-        throw new Error('Error to get users');
+        throw new Error('Error to get tasks');
       }
 
       const data = await response.json();
       setTasks(data);
 
     } catch (e) {
-      console.log('Error to get users: ', e);
+      console.log('Error to get tasks: ', e);
     }
   };
   useEffect(() => {
-    getTasks();
-  }, [setTasks])
+    if (user) { 
+        getTasks();
+    }
+}, [user]);
 
   return (
     <section>
       <Header title="Menu"/>
-      <Tasks tasks={tasks}/>
+      {user ? (
+            <>
+                <h1>Bem-vindo, {user.name_user}!</h1>
+                <Tasks tasks={tasks} />
+            </>
+        ) : (
+          <>
+            <p>usuário não carregou...</p>
+            <a href="http://localhost:5173/login">Voltar</a>
+          </>
+        )}
     </section>
   );
 }
