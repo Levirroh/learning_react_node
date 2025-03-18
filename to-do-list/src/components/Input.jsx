8a01a1ff62a1bd4bd3544d00eadaf0709a6d2137
Levirroh <johann.gr14@gmail.com>
2025-03-18 09:50:21 -0300

    update: task now accepts id & name

--- to-do-list/src/components/Input.jsx
+++ to-do-list/src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function Input({label, placeholder}) {
+function Input({label, placeholder, name, id}) {
     return(
         <div className="flex flex-col gap-5">
             <label>{label}</label>
-            <input type="text" placeholder={placeholder}  className="border-1 p-2 bg-gray-200 rounded-2xl"/>
+            <input type="text" id={id} name={name} placeholder={placeholder}  className="border-1 p-2 bg-gray-200 rounded-2xl"/>
         </div>
     );
 }
