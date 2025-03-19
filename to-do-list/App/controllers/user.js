9de703f6b6fb2f2659bbdd0005d28e1597a843d4
Levirroh <johann.gr14@gmail.com>
2025-03-19 10:58:20 -0300

    fix: minor changes

--- to-do-list/App/controllers/user.js
+++ to-do-list/App/controllers/user.js
@@ -38,36 +38,31 @@ export const register_user = async (req, res) => {
 
 export const login_user = async (req, res) => {
     const { login, password } = req.body;
-    try {
-        const query = "SELECT * FROM users WHERE name_user = ? OR email_user = ?";
-        const values = [login, login];
 
-        con.query(query, values, async (err, results) => {
-            if (err) {
-                console.error("Erro ao buscar usuário:", err);
-                return res.status(500).json({ error: "Erro ao entrar com usuário." });
-            }
+    const query = "SELECT * FROM users WHERE name_user = ? OR email_user = ?";
+    const values = [login, login];
 
-            if (results.length === 0) {
-                return res.status(401).json({ error: "Usuário não encontrado." });
-            }
+    con.query(query, values, (err, result) => {
+        if (err) {
+            console.error("Erro ao buscar usuário:", err);
+            return res.status(500).json({ error: "Erro ao entrar com usuário." });
+        }
 
-            const user = results[0];
+        if (result.length === 0) {
+            return res.status(401).json({ error: "Usuário não encontrado." });
+        }
 
-            if (password !== user.password_user) {
-                return res.status(401).json({ error: "Senha incorreta." });
-            }
+        const user = result[0]; 
 
-            res.status(200).json({
-                id: user.id,
-                name_user: user.name_user,
-                email_user: user.email_user,
-                function_user: user.function_user
-            });
+        if (user.password_user !== password) {
+            return res.status(401).json({ error: "Senha incorreta." });
+        }
+
+        res.status(200).json({
+            id_user: user.id_user, 
+            name_user: user.name_user,
+            email_user: user.email_user,
+            function_user: user.function_user
         });
-
-    } catch (error) {
-        console.error("Erro interno:", error);
-        res.status(500).json({ error: "Erro interno no servidor." });
-    }
-}
+    });
+};
