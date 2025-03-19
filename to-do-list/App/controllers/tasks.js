cd0c57a41ea4cb2f53d9129c432d4108a61891a3
Levirroh <johann.gr14@gmail.com>
2025-03-19 10:58:10 -0300

    feat: tasks shows only user tasks

--- to-do-list/App/controllers/tasks.js
+++ to-do-list/App/controllers/tasks.js
@@ -1,13 +1,18 @@
 import { con } from "../connection.js";
 
-export const getTasks = (_, res) => {
-    const query = "SELECT * FROM tasks";
+export const getTasks = (req, res) => {
+    const { id_user } = req.body;
+    const query = "SELECT * FROM tasks WHERE user_task = ?";
+    const values = id_user;
 
-    con.query(query, (err, data) => {
-        if (err) return res.json(err);
+    con.query(query, values, (err, data) => {
+        if (err) {
+            console.error("Erro ao buscar tarefas:", err);
+            return res.status(500).json({ error: "Erro ao buscar tarefas." });
+        }
 
-        return res.status(200).json(data)
-    })
+        return res.status(200).json(data);
+    });
 };
 
 
