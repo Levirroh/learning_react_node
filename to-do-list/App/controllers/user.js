0e3b3a95a2d53296c7da402198daa2c9fb64e2a6
Levirroh <johann.gr14@gmail.com>
2025-03-19 09:42:33 -0300

    feat: login_user

--- to-do-list/App/controllers/user.js
+++ to-do-list/App/controllers/user.js
@@ -34,3 +34,40 @@ export const register_user = async (req, res) => {
         res.status(500).json({ error: "Erro interno no servidor." });
     }
 }
+
+
+export const login_user = async (req, res) => {
+    const { login, password } = req.body;
+    try {
+        const query = "SELECT * FROM users WHERE name_user = ? OR email_user = ?";
+        const values = [login, login];
+
+        con.query(query, values, async (err, results) => {
+            if (err) {
+                console.error("Erro ao buscar usuário:", err);
+                return res.status(500).json({ error: "Erro ao entrar com usuário." });
+            }
+
+            if (results.length === 0) {
+                return res.status(401).json({ error: "Usuário não encontrado." });
+            }
+
+            const user = results[0];
+
+            if (password !== user.password_user) {
+                return res.status(401).json({ error: "Senha incorreta." });
+            }
+
+            res.status(200).json({
+                id: user.id,
+                name_user: user.name_user,
+                email_user: user.email_user,
+                function_user: user.function_user
+            });
+        });
+
+    } catch (error) {
+        console.error("Erro interno:", error);
+        res.status(500).json({ error: "Erro interno no servidor." });
+    }
+}
