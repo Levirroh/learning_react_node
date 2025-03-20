0fafc7f766e9d68304e9b52ac15675f6da1bc533
Levirroh <johann.gr14@gmail.com>
2025-03-20 09:25:32 -0300

    style: minor adjustaments

--- to-do-list/src/components/NewTask.jsx
+++ to-do-list/src/components/NewTask.jsx
@@ -5,7 +5,7 @@ import new_task from "../images/new_task.svg";
 function Task(){
     return(
         <div className={`flex flex-col border-1 p-2 rounded-2xl ToDo mt-3 text-left justify-center items-center h-35`}>
-            <a href="/task">
+            <a href="/task" className="w-full h-full flex justify-center items-center ">
                 <img src={new_task} alt="Nova tarefa" />
             </a>
         </div> 
