fb40bcbe3bf209cc30fd8f3c89ee6f465eb2eec9
Levirroh <johann.gr14@gmail.com>
2025-03-20 09:41:53 -0300

    feat: create task page minor style & components

--- to-do-list/src/Pages/CreateTask.jsx
+++ to-do-list/src/Pages/CreateTask.jsx
@@ -1,9 +1,22 @@
 import React from "react"; 
+import Input from "../components/Input"
+import Submit from "../components/Submit"
+import TextArea from "../components/TextArea"
 
 function CreateTask() {
     return(
-        <section>
-            <h1>Create Task</h1>
+        <section className="bg-slate-300 h-screen w-screen flex flex-col justify-center items-center">
+            <div className="bg-sky-300 h-7/10 w-4/10 rounded-2xl p-3">
+                <h1>Crie uma tarefa:</h1>
+                <form>
+                    <Input label="Título:" />
+                    <TextArea label="Descrição:"/>
+                    <Input label="Assunto:" />
+                    <div className="flex items-center justify-center">
+                        <Submit text="Criar" />
+                    </div>
+                </form>
+            </div>
         </section>
     );
 }
