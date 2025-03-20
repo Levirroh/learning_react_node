bee92a1f85b28ea3286611f0c9b4e1ba7ebad3a1
Levirroh <johann.gr14@gmail.com>
2025-03-20 09:23:50 -0300

    feat: CreateTask page

---
+++ to-do-list/src/Pages/CreateTask.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+function CreateTask() {
+    return(
+        <section>
+            <h1>Create Task</h1>
+        </section>
+    );
+}
+
+export default CreateTask;
