e7e281400c0b56c949e1848855b67def8a7afcdd
Levirroh <johann.gr14@gmail.com>
2025-03-23 21:31:26 -0300

    feat: create task logic first

--- to-do-list/src/Pages/CreateTask.jsx
+++ to-do-list/src/Pages/CreateTask.jsx
@@ -2,16 +2,50 @@ import React from "react";
 import Input from "../components/Input"
 import Submit from "../components/Submit"
 import TextArea from "../components/TextArea"
+import { useNavigate } from "react-router-dom";
+
 
 function CreateTask() {
+   const navigate = useNavigate();
+       
+    async function create(event){
+        event.preventDefault(); 
+        const formData = new FormData(event.target);
+        const title = formData.get("title");
+        const description = formData.get("description");
+        const subject = formData.get("subject");
+
+        try {
+
+            const response = await fetch("http://localhost:8800/newTask",  {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title, description, subject })
+        });
+            const taskData = await response.json();
+
+            if(!response.ok){
+                throw new Error('Error to create user');
+            }
+
+            alert("Cadastro realizado com sucesso!");
+            navigate("/menu")    
+
+        } catch (e) {
+            console.log('Error to create task: ', e);
+        }
+
+    }; 
     return(
         <section className="bg-slate-300 h-screen w-screen flex flex-col justify-center items-center">
             <div className="bg-sky-300 h-7/10 w-4/10 rounded-2xl p-3">
                 <h1>Crie uma tarefa:</h1>
-                <form>
-                    <Input label="Título:" />
-                    <TextArea label="Descrição:"/>
-                    <Input label="Assunto:" />
+                <form onSubmit={create}>
+                    <Input label="Título:" name="title"/>
+                    <TextArea label="Descrição:" name="description"/>
+                    <Input label="Assunto:" name="subject"/>
                     <div className="flex items-center justify-center">
                         <Submit text="Criar" />
                     </div>
