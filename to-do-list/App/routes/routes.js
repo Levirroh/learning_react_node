88b5696ad8bc454f357734533e8969e3ceadf7cc
Levirroh <johann.gr14@gmail.com>
2025-03-24 02:12:10 -0300

    feat: new_task calls new_task

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { getUsers, register_user, login_user} from "../controllers/user.js";
-import {getTasks, new_task} from "../controllers/tasks.js";
+import {get_tasks, new_task} from "../controllers/tasks.js";
 
 const router = express.Router();
 
-router.post("/getTasks", getTasks);
+router.post("/getTasks", get_tasks);
 
 router.post("/createUser", register_user);
 router.post("/loginUser", login_user)
