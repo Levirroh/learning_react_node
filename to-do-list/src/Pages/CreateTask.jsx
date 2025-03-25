2fccbd88cf1d9e0f9e90c630d8e21ecc3f39bfce
Levirroh <johann.gr14@gmail.com>
2025-03-25 10:10:34 -0300

    feat: minor changes

--- to-do-list/src/Pages/CreateTask.jsx
+++ to-do-list/src/Pages/CreateTask.jsx
@@ -6,8 +6,10 @@ import { useNavigate } from "react-router-dom";
 
 
 function CreateTask() {
-   const navigate = useNavigate();
-       
+    const navigate = useNavigate();
+    const storedUser = localStorage.getItem("user");
+    const id_user = storedUser ? JSON.parse(storedUser).id_user : null;
+
     async function create(event){
         event.preventDefault(); 
         const formData = new FormData(event.target);
@@ -22,12 +24,12 @@ function CreateTask() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ title, description, subject })
+            body: JSON.stringify({ title, description, subject, id_user })
         });
             const taskData = await response.json();
 
             if(!response.ok){
-                throw new Error('Error to create user');
+                throw new Error('Error to create task');
             }
 
             alert("Cadastro realizado com sucesso!");
@@ -38,6 +40,7 @@ function CreateTask() {
         }
 
     }; 
+
     return(
         <section className="bg-slate-300 h-screen w-screen flex flex-col justify-center items-center">
             <div className="bg-sky-300 h-7/10 w-4/10 rounded-2xl p-3">
@@ -46,7 +49,6 @@ function CreateTask() {
                     <Input label="Título:" name="title"/>
                     <TextArea label="Descrição:" name="description"/>
                     <Input label="Assunto:" name="subject"/>
-                    <input type="hidden" value={id_user} name="id_user" />
                     <div className="flex items-center justify-center">
                         <Submit text="Criar" />
                     </div>
