61de390673ee39be15544ced19e16b71dde7f967
Levirroh <johann.gr14@gmail.com>
2025-03-26 18:05:24 -0300

    fix: showing task pop-up

--- to-do-list/src/components/Task.jsx
+++ to-do-list/src/components/Task.jsx
@@ -1,38 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Task({ id, user, title, description, subject, date, status }) {
-    const [selectedTask, setSelectedTask] = useState(null);
-
+function Task({ id, user, title, description, subject, date, status, selectedTask, setSelectedTask }) {
     const showTask = () => {
-        setSelectedTask({ id, user, title, description, subject, date });
+        if (selectedTask?.id === id) {
+            setSelectedTask(null); 
+        } else {
+            setSelectedTask({ id, user, title, description, subject, date });
+        }
     };
 
     return (
-        <div>
-            <div 
-                className={`flex flex-col border-1 p-2 rounded-2xl ${status} mt-3 text-left cursor-pointer`} 
-                onClick={showTask}
-            >
-                id: {id} <br/>
-                user: {user}<br/>
-                title: {title} <br/>
-                description: {description}<br/>
-                subject: {subject} <br/>
-                {date}<br/>
-            </div> 
-
-            {selectedTask && (
-                <div id="task_selected">
-                    <h3 className="text-lg font-bold">Tarefa Selecionada</h3>
-                    <p><strong>ID:</strong> {selectedTask.id}</p>
-                    <p><strong>User:</strong> {selectedTask.user}</p>
-                    <p><strong>Title:</strong> {selectedTask.title}</p>
-                    <p><strong>Description:</strong> {selectedTask.description}</p>
-                    <p><strong>Subject:</strong> {selectedTask.subject}</p>
-                    <p><strong>Date:</strong> {selectedTask.date}</p>
-                </div>
-            )}
-        </div>
+        <div 
+            className={`flex flex-col border p-2 rounded-2xl ${status} mt-3 text-left cursor-pointer`} 
+            onClick={showTask}
+        >
+            <p>ID: {id}</p>
+            <p>User: {user}</p>
+            <p>Title: {title}</p>
+            <p>Description: {description}</p>
+            <p>Subject: {subject}</p>
+            <p>Date: {date}</p>
+        </div> 
     );
 }
 
