e0190b74007024e640fd41a18e0590a7dbe65d36
Levirroh <johann.gr14@gmail.com>
2025-03-26 18:05:31 -0300

    fix: task pop-up

--- to-do-list/src/components/Tasks.jsx
+++ to-do-list/src/components/Tasks.jsx
@@ -1,56 +1,85 @@
-import React from "react";
-import Task from "../components/Task"
-import NewTask from "../components/NewTask"
+import React, { useState } from "react";
+import Task from "../components/Task";
+import NewTask from "../components/NewTask";
 
 function Tasks({ tasks = [] }) {
+    const [selectedTask, setSelectedTask] = useState(null);
+
     return (
-        <section className="flex h-svh w-svw justify-evenly">
+        <section className="flex h-screen w-screen justify-evenly">
             <div id="ToDo" className="text-center w-1/4">
-                <p id="todo">TO DO</p>
+                <p>TO DO</p>
                 <ul>
                     <NewTask />
-                    {tasks.filter(task => task.status_task === "ToDo").map((task, index) => (
-                        <Task key={index}
+                    {tasks.filter(task => task.status_task === "ToDo").map((task) => (
+                        <Task key={task.id_task}
                             id={task.id_task}
                             user={task.user_task}
                             title={task.title_task}
                             description={task.description_task}
                             subject={task.subject_task}
                             date={task.date_task}
-                            status={task.status_task} />
+                            status={task.status_task}
+                            selectedTask={selectedTask}
+                            setSelectedTask={setSelectedTask}
+                        />
                     ))}
                 </ul>
             </div>
             <div id="Doing" className="text-center w-1/4">
                 <p>Doing</p>
                 <ul>
-                    {tasks.filter(task => task.status_task === "Doing").map((task, index) => (
-                        <Task key={index}
+                    {tasks.filter(task => task.status_task === "Doing").map((task) => (
+                        <Task key={task.id_task}
                             id={task.id_task}
                             user={task.user_task}
                             title={task.title_task}
                             description={task.description_task}
                             subject={task.subject_task}
                             date={task.date_task}
-                            status={task.status_task} />
+                            status={task.status_task}
+                            selectedTask={selectedTask}
+                            setSelectedTask={setSelectedTask}
+                        />
                     ))}
                 </ul>
             </div>
             <div id="Done" className="text-center w-1/4">
                 <p>Done</p>
                 <ul>
-                    {tasks.filter(task => task.status_task === "Done").map((task, index) => (
-                        <Task key={index}
+                    {tasks.filter(task => task.status_task === "Done").map((task) => (
+                        <Task key={task.id_task}
                             id={task.id_task}
                             user={task.user_task}
                             title={task.title_task}
                             description={task.description_task}
                             subject={task.subject_task}
                             date={task.date_task}
-                            status={task.status_task} />
+                            status={task.status_task}
+                            selectedTask={selectedTask}
+                            setSelectedTask={setSelectedTask}
+                        />
                     ))}
                 </ul>
             </div>
+            
+            {selectedTask && (
+                <div id="task_selected">
+                    <div>
+                        <h3 className="bold text-2xl">{selectedTask.title}</h3>
+                        <p>{selectedTask.id}</p>
+                    </div>
+                    <p>User: {selectedTask.user}</p>
+                    <p>Description: {selectedTask.description}</p>
+                    <p>Subject: {selectedTask.subject}</p>
+                    <button 
+                        className="mt-2 px-4 py-2 bg-red-500 text-white rounded"
+                        onClick={() => setSelectedTask(null)}
+                    >
+                        Fechar
+                    </button>
+                </div>
+            )}
         </section>
     );
 }
