88b47f67d149b741df4397b12c78da285f95ec3a
Levirroh <johann.gr14@gmail.com>
2025-03-30 21:25:15 -0300

    feat: update task on router

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUsers, register_user, login_user} from "../controllers/user.js";
-import {get_tasks, new_task} from "../controllers/tasks.js";
+import {get_tasks, new_task, update_task} from "../controllers/tasks.js";
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post("/getTasks", get_tasks);
 router.post("/createUser", register_user);
 router.post("/loginUser", login_user)
 router.post("/newTask", new_task)
+router.get("/updateTask", update_task)
+
+
 
 export default router;
