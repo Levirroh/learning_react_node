d23b3ee5c8bcf7bd1b5ffb7a548b5dd5cb19be04
Levirroh <johann.gr14@gmail.com>
2025-03-30 21:26:09 -0300

    feat: task gives task id to main

--- to-do-list/src/components/Tasks.jsx
+++ to-do-list/src/components/Tasks.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Task from "../components/Task";
 import NewTask from "../components/NewTask";
-
 function Tasks({ tasks = [] }) {
     const [selectedTask, setSelectedTask] = useState(null);
 
@@ -75,10 +74,17 @@ function Tasks({ tasks = [] }) {
                         <p>User: {selectedTask.user}</p>
                         <p>Description: {selectedTask.description}</p>
                         <p>Subject: {selectedTask.subject}</p>
-                        <button 
-                            className="mt-2 px-4 py-2 bg-slate-200 text-black rounded cursor-pointer"
-                            onClick={() => setSelectedTask(null)}
-                        >Fechar</button>
+                        <div className="flex items-center justify-center  w-1/2 gap-10">
+                            <a href={`/update/${selectedTask.id}`}>
+                                <button className="mt-2 px-4 py-2 bg-slate-200 text-black rounded cursor-pointer">
+                                    Atualizar
+                                </button>
+                            </a>
+                            <button
+                                className="mt-2 px-4 py-2 bg-red-400 text-black rounded cursor-pointer"
+                                onClick={() => setSelectedTask(null)}
+                            >Fechar</button>
+                        </div>
                     </div>
                 </div>
             )}
