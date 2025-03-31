13e3e1eeab36594967a5909439ccfe78b818f940
Levirroh <johann.gr14@gmail.com>
2025-03-30 21:25:52 -0300

    feat: input has onchange & receives value as prop

--- to-do-list/src/components/Input.jsx
+++ to-do-list/src/components/Input.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
+function Input({ label, placeholder, name, id, value }) {
+    const [inputValue, setInputValue] = useState(value);
+    if (!value) {
+        value = "..."}
 
-function Input({label, placeholder, name, id}) {
-    return(
+    return (
         <div className="flex flex-col gap-5">
             <label>{label}</label>
-            <input type="text" id={id} name={name} placeholder={placeholder}  className="border-1 p-2 bg-gray-200 rounded-2xl"/>
+            <input 
+                type="text" 
+                id={id} 
+                name={name} 
+                placeholder={placeholder} 
+                value={inputValue} 
+                onChange={(e) => setInputValue(e.target.value)}
+                className="border-1 p-2 bg-gray-200 rounded-2xl"
+            />
         </div>
     );
 }
