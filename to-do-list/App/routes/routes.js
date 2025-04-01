f68664a3e76c99be636a2727451c25f520dc3311
Levirroh <johann.gr14@gmail.com>
2025-03-31 22:47:07 -0300

    feat: getTaskById calls task.js function

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUsers, register_user, login_user} from "../controllers/user.js";
-import {get_tasks, new_task, update_task} from "../controllers/tasks.js";
+import {get_tasks, new_task, update_task, get_task_by_id} from "../controllers/tasks.js";
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.post("/createUser", register_user);
 router.post("/loginUser", login_user)
 router.post("/newTask", new_task)
 router.get("/updateTask", update_task)
+router.get("/getTaskById", get_task_by_id)
 
 
 
