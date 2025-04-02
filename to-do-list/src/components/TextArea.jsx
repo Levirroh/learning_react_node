6efb842440c8a45418a2c9f24fd0bd3936e197be
Levirroh <johann.gr14@gmail.com>
2025-04-01 23:05:08 -0300

    fix: minor changes

--- to-do-list/src/components/TextArea.jsx
+++ to-do-list/src/components/TextArea.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-function Input({label, placeholder, name, id}) {
+function Input({label, placeholder, name, id, value}) {
     return(
         <div className="flex flex-col gap-5">
             <label>{label}</label>
-            <textarea id={id} name={name} placeholder={placeholder}  className="border-1 p-2 bg-gray-200 rounded-2xl"></textarea>
+            <textarea id={id} name={name} placeholder={placeholder} value={value} onChange={(e) => setInputValue(e.target.value)} className="border-1 p-2 bg-gray-200 rounded-2xl"></textarea>
         </div>
     );
 }
