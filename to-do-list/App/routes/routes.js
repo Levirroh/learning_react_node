0ee8f96d06930348282398dfcf8f8acd06325a7f
Levirroh <johann.gr14@gmail.com>
2025-04-07 22:52:00 -0300

    update: new routers

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,17 +1,20 @@
 import express from "express";
 import { getUsers, register_user, login_user} from "../controllers/user.js";
-import {get_tasks, new_task, update_task, get_task_by_id, delete_task} from "../controllers/tasks.js";
+import {get_tasks, new_task, update_task, get_task_by_id, delete_task, change_status} from "../controllers/tasks.js";
 
 const router = express.Router();
 
 router.post("/getTasks", get_tasks);
-
 router.post("/createUser", register_user);
 router.post("/loginUser", login_user)
 router.post("/newTask", new_task)
 router.post("/updateTask", update_task)
+router.post("/changeStatus", change_status)
+
 router.get("/getTaskById/:id", get_task_by_id)
+
 router.delete("/delete/:id", delete_task)
+
 
 
 export default router;
