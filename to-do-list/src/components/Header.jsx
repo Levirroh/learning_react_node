79e42a7be2922d6ac2138bc6cd6f6e37c7c69454
Levirroh <johann.gr14@gmail.com>
2025-04-09 23:05:47 -0300

    feat: exit button

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Header({title}) {
     return (
-        <section className="bg-blue-400 flex items-center justify-center">
+        <section className="bg-blue-400 flex items-center justify-between p-4">
             <h1>{title}</h1>
+            <a href="/login"><button>Sair</button></a>
         </section>
     );
 }
