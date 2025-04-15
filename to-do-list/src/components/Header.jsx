5cbe815444ec5102d728aafe32395903b7fc6e78
Levirroh <johann.gr14@gmail.com>
2025-04-15 18:05:36 -0300

    fix: header receive toggle

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-function Header({title}) {
+function Header({ title, onToggleMenu }) {
     return (
-        <section className="bg-blue-400 flex items-center justify-between p-4">
-            <h1>{title}</h1>
-            <a href="/login"><button>Sair</button></a>
-        </section>
+      <section className="bg-blue-400 flex items-center justify-between p-4">
+        <button onClick={onToggleMenu}>{title}</button>
+        <a href="/login"><button>Sair</button></a>
+      </section>
     );
-}
-
-
-export default Header;
+  }
+  
+  export default Header;
