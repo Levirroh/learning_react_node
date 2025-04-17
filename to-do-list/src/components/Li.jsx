f982906c48bf41b4ee1d5b8755db86e6cdb04df4
Levirroh <johann.gr14@gmail.com>
2025-04-16 23:32:32 -0300

    update: link on li for full size link click

--- to-do-list/src/components/Li.jsx
+++ to-do-list/src/components/Li.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Li({ link, text }) {
     return (
-        <li className="border border-blue-600 bg-blue-500 rounded-2xl
-        h-12 flex items-center text-white p-3 mt-3 hover:cursor-pointer"><a href={`${link}`} >{text}</a></li>
+        <a href={`${link}`} ><li className="border border-blue-600 bg-blue-500 rounded-2xl
+        h-12 flex items-center text-white p-3 mt-3 hover:cursor-pointer">{text}</li></a>
     );
   }
   
