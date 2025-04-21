2fa43cd718c66fad5ae8c412f8d9872fd4fc4c57
Levirroh <johann.gr14@gmail.com>
2025-04-20 23:48:31 -0300

    feat: function to get personalized tasks status from team

--- to-do-list/src/components/TeamIcon.jsx
+++ to-do-list/src/components/TeamIcon.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function TeamIcon({nomeDoTime, id}) {
+
+function TeamIcon({nomeDoTime, id_team }) {
+    const [team, setTeam] = useState(null);
+    const navigate = useNavigate();
+
+    let aFazer = 0; 
+    let Fazendo = 0;
+    let Feitas = 0;
+    useEffect(() => {
+            async function getTasks() {
+            try {
+                const response = await fetch("http://localhost:8800/getStatusTeam", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ id_team })
+                });
+    
+                const data = await response.json();
+    
+                if (data.length === 0) {
+                    console.log("Nenhum status de time encontrado para este usuário.");
+                }
+    
+                setTeam(data);
+            } catch (e) {
+                console.error("Erro ao buscar tarefas:", e);
+            }
+            }
+    
+            getTasks(); 
+        }, []);
+
     return (
         <div 
             className={`flex flex-col border p-2 rounded-2xl mt-3 text-left cursor-pointer`} 
         >
           <h1>{nomeDoTime}</h1>
-          <p>{id}</p>
+          <p>{id_team}</p>
+          <p>{aFazer} / {Fazendo} / {Feitas}</p>
         </div> 
     );
 }
