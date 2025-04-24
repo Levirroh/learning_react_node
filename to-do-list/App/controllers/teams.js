bce88b79d87c4b3f1bc15bf49f7df93b787cf66f
Levirroh <johann.gr14@gmail.com>
2025-04-23 23:10:38 -0300

    feat: getTeamTask

--- to-do-list/App/controllers/teams.js
+++ to-do-list/App/controllers/teams.js
@@ -33,3 +33,20 @@ export const getStatusTeam = (req, res) => {
         return res.status(200).json(data);
     });
 };
+
+
+export const getTeamTasks = (req, res) => {
+    const { id } = req.body;
+
+    const query = "SELECT * FROM tasks INNER JOIN task_status ON task_status.id_status = tasks.status_task WHERE tasks.user_task = ? AND tasks.team_task IS NOT NULL";
+    const values = [id];
+
+    con.query(query, values, (err, data) => {
+        if (err) {
+            console.error("Erro ao buscar times:", err);
+            return res.status(500).json({ error: "Erro ao buscar tarefas", details: err });
+        }
+
+        return res.status(200).json(data);
+    });
+};
