698153d71852c310b9e5616c296ca535a3f7356b
Levirroh <johann.gr14@gmail.com>
2025-04-23 23:11:41 -0300

    fix: delete unused function

--- to-do-list/src/components/TeamIcon.jsx
+++ to-do-list/src/components/TeamIcon.jsx
@@ -2,53 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 
-function TeamIcon({nomeDoTime, id_team }) {
+function TeamIcon({nomeDoTime, id_team, funcao }) {
     const [team, setTeam] = useState(null);
     const navigate = useNavigate();
 
-    let aFazer = 0; 
-    let Fazendo = 0;
-    let Feitas = 0;
-    useEffect(() => {
-            async function getTasks() {
-            try {
-                const response = await fetch("http://localhost:8800/getStatusTeam", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ id_team })
-                });
-    
-                const data = await response.json();
-    
-                if (data.length === 0) {
-                    console.log("Nenhum status de time encontrado para este usuário.");
-                }
-                setTeam(data);
-
-                for (var i; i <= data.lenght; i++){
-                    if (data[i].status_task == 1){
-                        aFazer += 1;
-                    } else if(data[i].status_task == 2){
-                        Fazendo += 1;
-                    } else{
-                        Feitas += 1;
-                    }
-                }
-            } catch (e) {
-                console.error("Erro ao buscar tarefas:", e);
-            }
-            }
-    
-            getTasks(); 
-        }, []);
+    function goToTeam(){
+        navigate(`/Team/${id_team}`);
+    }
 
     return (
-        <div 
-            className={`flex flex-col border p-2 rounded-2xl mt-3 text-left cursor-pointer`} 
-        >
-          <h1>{nomeDoTime}</h1>
-          <p>{id_team}</p>
-          <p>{aFazer} / {Fazendo} / {Feitas}</p>
+        <div className={`flex flex-col border p-2 rounded-2xl mt-3 text-left cursor-pointer`} onClick={goToTeam}>
+          <h1>Time: {nomeDoTime}</h1>
+          <p>Função: {funcao}</p>
+          <p></p>
         </div> 
     );
 }
