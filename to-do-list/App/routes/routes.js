132be0c3f0bd8bef6d6716e651edc04883da054f
Levirroh <johann.gr14@gmail.com>
2025-04-27 22:19:37 -0300

    feat: new route to new team task

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUsers, register_user, login_user} from "../controllers/user.js";
-import { getUserTeams, getStatusTeam, getTeamTasks} from "../controllers/teams.js";
+import { getUserTeams, getStatusTeam, getTeamTasks, new_team_task} from "../controllers/teams.js";
 import {get_tasks_user, new_task, update_task, get_task_by_id, delete_task, change_status} from "../controllers/tasks.js";
 
 const router = express.Router();
@@ -15,6 +15,7 @@ router.post("/getSubjects/", register_user);
 router.post("/getUserTeams/", getUserTeams);
 router.post("/getStatusTeam/", getStatusTeam);
 router.post("/getTeamTasks", getTeamTasks);
+router.post("/newTeamTask", new_team_task);
 
 
 router.get("/getTaskById/:id", get_task_by_id)
