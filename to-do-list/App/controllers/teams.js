ae5498024af4337f43a477c73336eac19a79df1f
Levirroh <johann.gr14@gmail.com>
2025-04-27 22:19:27 -0300

    feat: new tem task

--- to-do-list/App/controllers/teams.js
+++ to-do-list/App/controllers/teams.js
@@ -18,6 +18,29 @@ export const getUserTeams = (req, res) => {
 };
 
 
+export const new_task = (req, res) => {
+    const { title, description, subject, id_user, id_team } = req.body;
+
+    if (!title || !description || !subject || !id_user) {
+        return res.status(400).json({ error: "Todos os campos são obrigatórios!" });
+    }
+
+    const query = "INSERT INTO tasks (user_task, title_task, description_task, subject_task, status_task, team_task) VALUES (?, ?, ?, ?, ?, ?)";
+    const values = [id_user, title, description, subject, "ToDo", id_team];
+
+    con.query(query, values, (err, result) => {
+        if (err) {
+            console.error("Erro ao criar tarefa:", err);
+            return res.status(500).json({ error: "Erro ao criar tarefa no banco de dados." });
+        }
+
+        res.status(201).json({ id: result.insertId, message: "Tarefa criada com sucesso!" });
+    });
+};
+
+
+
+
 export const getStatusTeam = (req, res) => {
     const { id_team } = req.body;
 
