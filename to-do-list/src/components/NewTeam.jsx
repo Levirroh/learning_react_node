cbc408c1ec770de935551dd3109819ab69707b88
Levirroh <johann.gr14@gmail.com>
2025-04-29 17:16:51 -0300

    fix: NewTeam component

--- to-do-list/src/components/NewTeam.jsx
+++ to-do-list/src/components/NewTeam.jsx
@@ -5,13 +5,11 @@ import team_icon from "../images/team_icon.png";
 
 function NewTeam(){
     return(
-        <div className={`flex flex-col border-1 p-2 rounded-2xl ToDo mt-3 text-left justify-center items-center h-25 w-1/6`}>
-            <a href="/task" className="w-full h-full flex justify-center items-center ">
+        <div className={`flex flex-col border-1 p-2 rounded-2xl ToDo mt-3 text-left justify-center items-center h-25 w-1/6 hover:cursor-pointer`}>
             <div className="flex justify-center items-center">
                 <img src={team_icon} className="h-20" />
                 <img src={new_task} alt="Nova tarefa" className="h-13" />
             </div>
-            </a>
         </div> 
 );
 }
