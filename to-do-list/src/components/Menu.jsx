cd51864a466675ac2bcfadf98e772ad613bb4def
Levirroh <johann.gr14@gmail.com>
2025-04-30 18:11:45 -0300

    style: cursor

--- to-do-list/src/components/Menu.jsx
+++ to-do-list/src/components/Menu.jsx
@@ -6,7 +6,7 @@ function Menu({ onClose, isOpen }) {
       <div
         className={`fixed top-0 left-0 w-64 h-full bg-blue-400 shadow-lg shadow-black p-4 z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
-        <button onClick={onClose} className="mb-4">Fechar</button>
+        <button onClick={onClose} className="mb-4 cursor-pointer">Fechar</button>
         <div className="h-11/12">
           <div>
             <Li text="Dashboard" link={"/dashboard"} />
