667917f697f01d56e9a9627dec18d7287b3f1c53
Levirroh <johann.gr14@gmail.com>
2025-04-30 18:11:55 -0300

    feat: pop-Up functions

--- to-do-list/src/components/NewTeam.jsx
+++ to-do-list/src/components/NewTeam.jsx
@@ -3,9 +3,12 @@ import new_task from "../images/new_task.svg";
 import team_icon from "../images/team_icon.png";
 
 
-function NewTeam(){
+function NewTeam({popUp, setPopUp}){
+    function newTeamPopUp() {
+        setPopUp(!popUp);
+    }
     return(
-        <div className={`flex flex-col border-1 p-2 rounded-2xl ToDo mt-3 text-left justify-center items-center h-25 w-1/6 hover:cursor-pointer`}>
+        <div className={`flex flex-col border-1 p-2 rounded-2xl ToDo mt-3 text-left justify-center items-center h-25 w-1/6 hover:cursor-pointer`} onClick={newTeamPopUp}>
             <div className="flex justify-center items-center">
                 <img src={team_icon} className="h-20" />
                 <img src={new_task} alt="Nova tarefa" className="h-13" />
