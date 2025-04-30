18a9274a280241b5c83068414c5455fd0e90251f
Levirroh <johann.gr14@gmail.com>
2025-04-30 18:12:37 -0300

    feat: functions for popUp & createTeam

--- to-do-list/src/Pages/Teams.jsx
+++ to-do-list/src/Pages/Teams.jsx
@@ -10,9 +10,17 @@ function Teams() {
     const [isMenuOpen, setIsMenuOpen] = useState(false); 
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
-    
+    const [popUp, setPopUp] = useState(false);
+    const [newTeamName, setNewTeamName] = useState("");
+
+
+
     function toggleMenu() {
         setIsMenuOpen(prev => !prev);
+    }
+
+    function newTeamPopUp() {
+        setPopUp(!popUp);
     }
 
     useEffect(() => {
@@ -24,6 +32,32 @@ function Teams() {
           navigate("/login");
         }
       }, [navigate]);
+
+
+    async function createTeam() {
+        const newTeamName = document.getElementById("newTeamName").value;
+        if (user){
+            try {
+                const response = await fetch("http://localhost:8800/createTeam", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name: newTeamName , owner: user.id_user })
+                });
+    
+                const data = await response.json();
+    
+                if (data.length === 0) {
+                    console.log("Nenhum time encontrado para este usuário.");
+                }
+        
+            } catch (e) {
+                console.error("Erro ao criar time:", e);
+            }
+        };
+        setPopUp(false);
+        setNewTeamName(""); 
+        //getUserTeams(); 
+    }
 
     useEffect(() => {
         async function getUserTeams() {
@@ -54,38 +88,47 @@ function Teams() {
         <section>
             <Header title="Teams" onToggleMenu={toggleMenu} />
             <Menu isOpen={isMenuOpen} onClose={toggleMenu} />
-            <h1>Times</h1>
-            <p>Seus times:</p>
-            <div className='flex gap-5'>
-                {teams.map((team) => (
-                        <TeamIcon key={team.id_team_members}
-                        nomeDoTime={team.name_team}
-                        id_team={team.id_team}
-                        funcao={team.role_user}/>
-                    ))}
-                {/* Deve ser feito lógica para um scroll ou setinhas para ver mais pra direita ou esquerda */ }
-            </div>
-            <p>Criar novo time</p>
-            <div className=''>
-                <div className='p-3 bg-black w-5'></div>
-                <NewTeam />
-                {/* Aqui será um botão para criar um novo time */ }
-            </div>
-            <small>Templates de times prontos</small>
-            <div className='flex gap-5'>
-                {/* Aqui será a lista de times prontos */ }
-                <div className='p-3 bg-black'></div>
-                <div className='p-3 bg-black'></div>
-                <div className='p-3 bg-black'></div>
-                <div className='p-3 bg-black'></div>
-                <div className='p-3 bg-black'></div>
-                {/* Deve ser feito lógica para um scroll ou setinhas para ver mais pra direita ou esquerda */ }
-            </div>
+            <section className='pl-3'>
 
-            <div className='absolute flex top-0 h-screen w-screen p-2 justify-center items-center'>
-                <label htmlFor="" className='bg-white p-2'>Nome do novo time:</label>
-                <input className='bg-white p-2' placeholder='Escola'/>
-            </div>
+                <h1>Times</h1>
+                <p>Seus times:</p>
+                <div className='flex gap-5'>
+                    {teams.map((team) => (
+                            <TeamIcon key={team.id_team_members}
+                            nomeDoTime={team.name_team}
+                            id_team={team.id_team}
+                            funcao={team.role_user}/>
+                        ))}
+                    {/* Deve ser feito lógica para um scroll ou setinhas para ver mais pra direita ou esquerda */ }
+                </div>
+                <p>Criar novo time</p>
+                <div className='min-w-300'>
+                    <NewTeam popUp={popUp} setPopUp={setPopUp}/>
+                </div>
+                <small>Templates de times prontos</small>
+                <div className='flex gap-5'>
+                    <div className='p-3 bg-black'></div>
+                    <div className='p-3 bg-black'></div>
+                    <div className='p-3 bg-black'></div>
+                    <div className='p-3 bg-black'></div>
+                    <div className='p-3 bg-black'></div>
+                </div>
+            </section>
+
+            {popUp && (<div className='absolute flex top-0 h-screen w-screen p-2 justify-center items-center bg-black opacity-45'>
+                <div className='bg-blue-700 h-[17vh] w-[53vw] rounded-2xl'>
+                    <div className='flex justify-end w-full pr-6 pt-2'>
+                        <button className='text-black font-bold cursor-pointer' onClick={newTeamPopUp}>X</button>
+                    </div>
+                    <div className='flex items-center justify-center h-[6vh]'>
+                        <label htmlFor="newTeamName" className='bg-white p-2'>Nome do novo time:</label>
+                        <input className='bg-white p-2' name='newTeamName' id="newTeamName" placeholder='Ex.: escola' onChange={(e) => setNewTeamName(e.target.value)}/>
+                    </div>
+                    <div className='flex justify-center mt-2'>
+                        <button className='bg-blue-400 rounded-sm pt-2 pb-2 pl-5 pr-5' onClick={createTeam}>Criar</button>
+                    </div>
+                </div>
+            </div>)};
         </section>
     );
 }
