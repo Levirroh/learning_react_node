62553f5824b2fdf2b54503343c9f9428f94aa751
Levirroh <johann.gr14@gmail.com>
2025-04-30 18:11:38 -0300

    style: cursor

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header({ title, onToggleMenu }) {
     return (
       <section className="bg-blue-400 flex items-center justify-between p-4">
-        <button onClick={onToggleMenu}>{title}</button>
+        <button onClick={onToggleMenu} className="cursor-pointer">{title}</button>
         <a href="/login"><button>Sair</button></a>
       </section>
     );
