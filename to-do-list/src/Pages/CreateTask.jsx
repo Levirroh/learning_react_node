3a7e749a635cb5a408edc36d27c26f665ee58c29
Levirroh <johann.gr14@gmail.com>
2025-05-02 19:33:51 -0300

    feat: create tasks for team and singular user are apart

--- to-do-list/src/Pages/CreateTask.jsx
+++ to-do-list/src/Pages/CreateTask.jsx
@@ -2,14 +2,77 @@ import React, { useEffect, useState } from 'react';
 import Input from "../components/Input"
 import Submit from "../components/Submit"
 import TextArea from "../components/TextArea"
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 
 function CreateTask() {
     const navigate = useNavigate();
     const storedUser = localStorage.getItem("user");
     const id_user = storedUser ? JSON.parse(storedUser).id_user : null;
+    const { id } = useParams();
+    async function create(event){
+        if (id){
+            event.preventDefault(); 
+            const formData = new FormData(event.target);
+            const title = formData.get("title");
+            const description = formData.get("description");
+            const subject = formData.get("subject");
 
+            try {
+
+                const response = await fetch("http://localhost:8800/newTeamTask",  {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title, description, subject, id_user, id })
+            });
+                const taskData = await response.json();
+
+                if(!response.ok){
+                    throw new Error('Error to create task');
+                }
+
+                if (taskData){
+                    alert("Cadastro realizado com sucesso!");
+                    navigate(`/Team/${id}`)    
+                }
+
+            } catch (e) {
+                console.log('Error to create task: ', e);
+            }
+        } else {
+            event.preventDefault(); 
+            const formData = new FormData(event.target);
+            const title = formData.get("title");
+            const description = formData.get("description");
+            const subject = formData.get("subject");
+
+            try {
+
+                const response = await fetch("http://localhost:8800/newTask",  {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title, description, subject, id_user })
+            });
+                const taskData = await response.json();
+
+                if(!response.ok){
+                    throw new Error('Error to create task');
+                }
+
+                if (taskData){
+                    alert("Cadastro realizado com sucesso!");
+                    navigate(`/menu`)    
+                }
+
+            } catch (e) {
+                console.log('Error to create task: ', e);
+            }
+        }
+    };
     // ainda nao pode ser usado pois o codigo nao tem
     // como saber se é um team ou user sozinho
 
@@ -34,38 +97,6 @@ function CreateTask() {
     //     }
 
     // });
-
-
-    async function create(event){
-        event.preventDefault(); 
-        const formData = new FormData(event.target);
-        const title = formData.get("title");
-        const description = formData.get("description");
-        const subject = formData.get("subject");
-
-        try {
-
-            const response = await fetch("http://localhost:8800/newTask",  {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ title, description, subject, id_user })
-        });
-            const taskData = await response.json();
-
-            if(!response.ok){
-                throw new Error('Error to create task');
-            }
-
-            alert("Cadastro realizado com sucesso!");
-            navigate("/menu")    
-
-        } catch (e) {
-            console.log('Error to create task: ', e);
-        }
-
-    }; 
 
     return(
         <section className="bg-slate-300 h-screen w-screen flex flex-col justify-center items-center">
