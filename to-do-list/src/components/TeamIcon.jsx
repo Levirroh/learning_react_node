7de4bd28dad40c64d90163216cf882f50483b06a
Levirroh <johann.gr14@gmail.com>
2025-05-03 22:38:38 -0300

    feat: teamIcon has config and more useState

--- to-do-list/src/components/TeamIcon.jsx
+++ to-do-list/src/components/TeamIcon.jsx
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import config_icon from "../images/config_icon.svg";
 
-
-function TeamIcon({nomeDoTime, id_team, funcao }) {
-    const [team, setTeam] = useState(null);
+function TeamIcon({nomeDoTime, id_team, funcao, openConfig, setOpenConfig, setSelectedTeamConfig }) {
     const navigate = useNavigate();
 
     function goToTeam(){
         navigate(`/Team/${id_team}`);
     }
 
+    function OpenConfigo(){
+        setOpenConfig(!openConfig);
+        setSelectedTeamConfig([id_team, nomeDoTime, funcao])
+    }
+
     return (
-        <div className={`flex flex-col border p-2 rounded-2xl mt-3 text-left cursor-pointer`} onClick={goToTeam}>
-          <h1>Time: {nomeDoTime}</h1>
-          <p>Função: {funcao}</p>
-          <p></p>
+        <div className={`flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
+            <div onClick={goToTeam} className="w-full flex flex-col justify-evenly">
+                <h1 className="w-[90%]">{nomeDoTime}</h1>
+                <p>{funcao}</p>
+            </div>
+            <div className="flex">
+                <img src={config_icon} alt="Configurações" className="h-5" onClick={OpenConfigo} />
+            </div>
         </div> 
     );
 }
