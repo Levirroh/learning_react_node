6a23684700541360378aad6c54e687d65e931a95
Levirroh <johann.gr14@gmail.com>
2025-05-18 21:23:32 -0300

    feat: new header for team

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-function Header({ title, onToggleMenu }) {
+function Header({ title, onToggleMenu, team }) {
+  if (team == true){
+    return (
+      <section className="bg-green-200 flex items-center justify-between p-4">
+        <button onClick={onToggleMenu} className="cursor-pointer">{title}</button>
+        <a href="/login"><button>Sair</button></a>
+      </section>
+    );
+  } else {
     return (
       <section className="bg-blue-400 flex items-center justify-between p-4">
         <button onClick={onToggleMenu} className="cursor-pointer">{title}</button>
         <a href="/login"><button>Sair</button></a>
       </section>
     );
+  }
   }
   
   export default Header;
