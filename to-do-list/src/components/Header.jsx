57c955b15f10b2158f02462e33522b5dab62c942
Levirroh <johann.gr14@gmail.com>
2025-05-19 23:05:38 -0300

    feat: different header on teams

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 function Header({ title, onToggleMenu, team }) {
+  function abrirMenu(){
+    console.log("Abrir menu")
+  }
   if (team == true){
     return (
       <section className="bg-green-200 flex items-center justify-between p-4">
         <button onClick={onToggleMenu} className="cursor-pointer">{title}</button>
-        <a href="/login"><button>Sair</button></a>
+        <div className="w-40 flex justify-between" onClick={abrirMenu}>
+          <button className="cursor-pointer">
+            Abrir menu
+          </button>
+          <a href="/login"><button>Sair</button></a>
+        </div>
       </section>
     );
   } else {
