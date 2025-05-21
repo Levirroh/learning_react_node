a5caf16213e917451acefd7c850e4afbe823589b
Levirroh <johann.gr14@gmail.com>
2025-05-20 22:32:48 -0300

    feat: new components for teams

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
 function Header({ title, onToggleMenu, team }) {
+    const [OpenMenu, setOpenMenu] = useState(false);
+
   function abrirMenu(){
-    console.log("Abrir menu")
+    setOpenMenu(!OpenMenu);
   }
   if (team == true){
     return (
       <section className="bg-green-200 flex items-center justify-between p-4">
         <button onClick={onToggleMenu} className="cursor-pointer">{title}</button>
-        <div className="w-40 flex justify-between" onClick={abrirMenu}>
-          <button className="cursor-pointer">
-            Abrir menu
-          </button>
+        <div className="w-50 flex justify-between" onClick={abrirMenu}>
+          <div className=''>
+            <button className="cursor-pointer w-20">
+            Menu
+            </button>
+            {OpenMenu && 
+              (<div className="absolute bg-green-200 w-45 flex flex-col">  
+                <a href="">Configurações do time</a>
+                <a href="">Usuários</a>
+                <a href="">Chat</a>
+              </div>)}
+          </div>
           <a href="/login"><button>Sair</button></a>
         </div>
       </section>
+
     );
   } else {
     return (
