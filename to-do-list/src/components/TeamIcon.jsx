42891a2895fc11af0ee0e9f3577b3afc66c3e1cd
Levirroh <johann.gr14@gmail.com>
2025-05-24 17:46:28 -0300

    feat: teamIcon personalized color

--- to-do-list/src/components/TeamIcon.jsx
+++ to-do-list/src/components/TeamIcon.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import config_icon from "../images/config_icon.svg";
 
-function TeamIcon({nomeDoTime, id_team, funcao, openConfig, setOpenConfig, setSelectedTeamConfig, getTeamMembers }) {
+function TeamIcon({nomeDoTime, id_team, funcao, openConfig, setOpenConfig, setSelectedTeamConfig, getTeamMembers, color }) {
     const navigate = useNavigate();
 
     function goToTeam(){
@@ -20,7 +20,7 @@ function TeamIcon({nomeDoTime, id_team, funcao, openConfig, setOpenConfig, setSe
 
     if(funcao == "Administrador"){
         return (
-            <div className={`flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
+            <div className={`${color} flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
                 <div onClick={goToTeam} className="w-full flex flex-col justify-evenly">
                     <h1 className="w-[90%]">{nomeDoTime}</h1>
                     <p>{funcao}</p>
@@ -32,7 +32,7 @@ function TeamIcon({nomeDoTime, id_team, funcao, openConfig, setOpenConfig, setSe
         );
     } else {
         return (
-            <div className={`flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
+            <div className={`${color} flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
                 <div onClick={goToTeam} className="w-full flex flex-col justify-evenly">
                     <h1 className="w-[90%]">{nomeDoTime}</h1>
                     <p>{funcao}</p>
