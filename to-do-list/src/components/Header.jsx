b37724b2ad5ecd9e15209d6600c09da48a07e785
Levirroh <johann.gr14@gmail.com>
2025-06-04 22:36:49 -0300

    style: header modal

--- to-do-list/src/components/Header.jsx
+++ to-do-list/src/components/Header.jsx
@@ -10,13 +10,13 @@ function Header({ title, onToggleMenu, team, color }) {
     return (
       <section className={`${color} flex items-center justify-between p-4`}>
         <button onClick={onToggleMenu} className="cursor-pointer">{title}</button>
-        <div className="w-50 flex justify-evenly" onClick={abrirMenu}>
+        <div className="w-50 flex justify-evenly relative" onClick={abrirMenu}>
           <div className=''>
             <button className="cursor-pointer w-20">
             Menu
             </button>
             {OpenMenu && 
-          (<div className={`absolute ${color} w-45 flex flex-col`}>  
+          (<div className={`absolute top-5 right-26 ${color} border border-gray-400 w-45 flex flex-col p-2 rounded-bl-md rounded-br-md rounded-tl-md shadow-2xl`}>  
                 <a href="" className='hover:bg-white transition'>Configurações do time</a>
                 <a href="" className='hover:bg-white transition'>Usuários</a>
                 <a href="" className='hover:bg-white transition'>Chat</a>
