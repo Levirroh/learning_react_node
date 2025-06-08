3ae1f4457b89e446b9951ae1953d88267910ba0c
Levirroh <johann.gr14@gmail.com>
2025-06-07 22:20:59 -0300

    fix: useless function

--- to-do-list/App/controllers/user.js
+++ to-do-list/App/controllers/user.js
@@ -10,31 +10,6 @@ export const getUsers = (_, res) => {
     })
 };
 
-export const register_user = async (req, res) => {
-    const { name_user, function_user, email_user, password_user } = req.body;
-    if (!name_user || !function_user || !email_user || !password_user) {
-        return res.status(400).json({ error: "Todos os campos são obrigatórios!" });
-    }
-    try {
-        const query = "INSERT INTO users (name_user, function_user, email_user, password_user) VALUES(?,?,?,?)";
-        const values = [name_user, function_user, email_user, password_user];
-
-        con.query(query, values, (err, result) => {
-                if (err) {
-                    console.error("Erro ao inserir usuário:", err);
-                    return res.status(500).json({ error: "Erro ao cadastrar usuário." });
-                }
-
-                res.status(201).json({ id: result.insertId, name_user, function_user, email_user });
-            });
-
-
-    } catch (error) {
-        console.error("Erro ao cadastrar usuário:", error);
-        res.status(500).json({ error: "Erro interno no servidor." });
-    }
-}
-
 
 export const login_user = async (req, res) => {
     const { login, password } = req.body;
