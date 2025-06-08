49dab94a73bcaf3ed507e122264a55a54715e686
Levirroh <johann.gr14@gmail.com>
2025-06-07 22:21:12 -0300

    feat: organization & new route to chats

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,31 +1,42 @@
 import express from "express";
-import { getUsers, register_user, login_user} from "../controllers/user.js";
+import { login_user} from "../controllers/user.js";
 import { getUserTeams, getStatusTeam, getTeamTasks, new_team_task, createTeam, getTeamMembers, getTeamData, updateTeam} from "../controllers/teams.js";
 import {get_tasks_user, new_task, update_task, get_task_by_id, delete_task, change_status} from "../controllers/tasks.js";
+import {get_chats_user} from "../controllers/chats.js";
+
 
 const router = express.Router();
 
+//tasks
 router.post("/getUserTasks", get_tasks_user);
-router.post("/createUser", register_user);
-router.post("/loginUser", login_user)
-router.post("/newTask", new_task)
 router.post("/updateTask", update_task)
 router.post("/changeStatus", change_status)
-router.post("/getSubjects/", register_user);
-router.post("/getUserTeams/", getUserTeams);
-router.post("/getStatusTeam/", getStatusTeam);
-router.post("/getTeamTasks", getTeamTasks);
+router.post("/newTask", new_task)
+router.get("/getTaskById/:id", get_task_by_id)
+router.delete("/delete/:id", delete_task)
+
+//team taks
 router.post("/newTeamTask", new_team_task);
+router.post("/getTeamTasks", getTeamTasks);
+
+//user related
+router.post("/createUser", register_user);
+router.post("/loginUser", login_user)
+
+//teams related
+router.post("/getStatusTeam/", getStatusTeam);
+router.post("/getUserTeams/", getUserTeams);
 router.post("/createTeam", createTeam);
 router.post("/getTeamMembers", getTeamMembers)
 router.post("/updateTeam", updateTeam)
 router.post("/getTeamData", getTeamData);
 
+//chat related
 
-router.get("/getTaskById/:id", get_task_by_id)
+router.post("/chats", get_chats_user)
 
 
-router.delete("/delete/:id", delete_task)
+
 
 
 
