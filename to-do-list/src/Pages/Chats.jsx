b0cc986c428bc1eb1d8782e86fc3ef80bead2ef0
Levirroh <johann.gr14@gmail.com>
2025-06-08 21:29:11 -0300

    fix: chat.jsx

--- to-do-list/src/Pages/Chats.jsx
+++ to-do-list/src/Pages/Chats.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import Header from '../components/Header';
 import Menu from "../components/Menu";
-import { useNavigate } from "react-router-dom";
-import TeamIcon from "../components/TeamIcon"
-import NewTeam from "../components/NewTeam"
+import ChatIcon from '../components/ChatIcon';
+import React, { useEffect, useState } from 'react';
+
 
 function Chats() {
-
+  const [chats, setChats] = useState([]);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
@@ -17,40 +19,41 @@ function Chats() {
         navigate("/login");
         }
     }, [navigate]);
+    useEffect(() => {
+        async function getUserChats() {
+            if (user){
+                try {
+                    const response = await fetch("http://localhost:8800/getUserChats", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ id_user: user.id_user })
+                    });
 
-    async function getUserChats() {
-        if (user){
-            try {
-                const response = await fetch("http://localhost:8800/getUserChats", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ id_user: user.id_user })
-                });
+                    const data = await response.json();
 
-                const data = await response.json();
-
-                if (data.length === 0) {
-                    console.log("Nenhum chat encontrado para este usuário.");
-                    }
-        
-                setTeams(data);
-            } catch (e) {
-                console.error("Erro ao buscar chats:", e);
+                    if (data.length === 0) {
+                        console.log("Nenhum chat encontrado para este usuário.");
+                        }
+            
+                    setChats(data);
+                } catch (e) {
+                    console.error("Erro ao buscar chats:", e);
+                }
             }
         }
-    }
+        console.log(chats);
+        getUserChats();
+    }, []);
 
 
     return(
         <section>
-            <Header title="Your chats" onToggleMenu={toggleMenu} />
-            <Menu isOpen={isMenuOpen} onClose={toggleMenu} />
             {chats.map((chat) => (
-                            <ChatIcon key={chat.id_chat}
-                            nomeDoTime={chat.name_chat}
-                            description={chat.description_chat}
-                            />
-                        ))}
+                <ChatIcon key={chat.id_chat}
+                nomeDoTime={chat.name_chat}
+                description={chat.description_chat}
+                />
+            ))}
         </section>
     )
 }
