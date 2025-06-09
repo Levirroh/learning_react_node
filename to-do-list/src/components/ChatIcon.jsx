92b9e23d77349477d19b2041542f851cdeb9fd56
Levirroh <johann.gr14@gmail.com>
2025-06-08 21:29:02 -0300

    feat: chat icon

--- to-do-list/src/components/ChatIcon.jsx
+++ to-do-list/src/components/ChatIcon.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import config_icon from "../images/config_icon.svg";
 
-function ChatIcon({nomeDoTime}) {
-
+function ChatIcon({nomeDoTime, description}) {
     return (
-        <div className={`${color} flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
-            <div onClick={goToTeam} className="w-full flex flex-col justify-evenly">
-                <h1 className="w-[90%]">{nomeDoTime}</h1>
-                <p>{funcao}</p>
+        <div className={`flex border p-2 rounded-2xl mt-3 text-left cursor-pointer items-top min-w-50`}>
+            <div className="w-full flex flex-col justify-evenly">
+                <h1 className="w-[90%]">nome: {nomeDoTime}</h1>
+                <p>descricao: {description}</p>
             </div>
         </div> 
     );
