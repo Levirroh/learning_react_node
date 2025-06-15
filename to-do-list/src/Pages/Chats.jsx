0841315887b3b9660773853b3f6166618c250ffd
Levirroh <johann.gr14@gmail.com>
2025-06-14 23:48:40 -0300

    feat: Chats showing chats

--- to-do-list/src/Pages/Chats.jsx
+++ to-do-list/src/Pages/Chats.jsx
@@ -3,13 +3,19 @@ import Header from '../components/Header';
 import Menu from "../components/Menu";
 import ChatIcon from '../components/ChatIcon';
 import React, { useEffect, useState } from 'react';
+import Chat from '../components/Chat';
 
 
 function Chats() {
   const [chats, setChats] = useState([]);
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+ const [isMenuOpen, setIsMenuOpen] = useState(false); 
 
+
+  function toggleMenu() {
+    setIsMenuOpen(prev => !prev);
+  }
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
         if (storedUser) {
@@ -34,7 +40,7 @@ function Chats() {
                     if (data.length === 0) {
                         console.log("Nenhum chat encontrado para este usuário.");
                         }
-            
+                    console.log(data);
                     setChats(data);
                 } catch (e) {
                     console.error("Erro ao buscar chats:", e);
@@ -43,17 +49,27 @@ function Chats() {
         }
         console.log(chats);
         getUserChats();
-    }, []);
+        }, [user]);
 
 
     return(
         <section>
-            {chats.map((chat) => (
-                <ChatIcon key={chat.id_chat}
-                nomeDoTime={chat.name_chat}
-                description={chat.description_chat}
-                />
-            ))}
+            <Header title="Menu" onToggleMenu={toggleMenu} />
+            <Menu isOpen={isMenuOpen} onClose={toggleMenu} />
+            <div className="flex w-full">
+                <div className="w-[30%]">
+                    {chats.map((chat) => (
+                        <ChatIcon key={chat.id_chat}
+                        nomeDoTime={chat.name_chat}
+                        description={chat.description_chat}
+                        />
+                    ))}
+                </div>
+                <div className="w-full">
+                    <Chat/>
+                </div>
+
+            </div>
         </section>
     )
 }
