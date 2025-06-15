96c201d7b9b79694a276746b8b47eca52143fa06
Levirroh <johann.gr14@gmail.com>
2025-06-14 23:48:15 -0300

    feat: chat messages component

--- to-do-list/src/components/Chat.jsx
+++ to-do-list/src/components/Chat.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 
-function Chat(id, name, description) {
-    
+function Chat(chatId) {
+    const [messages, setMessages] = useState([]);
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+        const parsedUser = JSON.parse(storedUser);
+        setUser(parsedUser);
+        } else {
+        navigate("/login");
+        }
+    }, [navigate]);
+
+
+    useEffect(() => {
+        // pegar todos os dados e mensagens do time.
+    }, [navigate]);
     return (
-        <div>
-            <p>ID: {id}</p>
-            <p>Nome do chat: {name}</p>
-            <p>Descricao: {description}</p>
+        <div className="w-full">
+            <p>Mensagens</p>
         </div> 
     );
 }
