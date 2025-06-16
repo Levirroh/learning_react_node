1afd746fb2054c93082491674074423aa2fd3fa0
Levirroh <johann.gr14@gmail.com>
2025-06-15 22:52:07 -0300

    feat: selected chat by clicking on icon

--- to-do-list/src/components/ChatIcon.jsx
+++ to-do-list/src/components/ChatIcon.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import config_icon from "../images/config_icon.svg";
 
-function ChatIcon({nomeDoTime, description}) {
+function ChatIcon({idChat, nomeDoTime, description, selectedChat, setSelectedChat}) {
     return (
-        <div className={`flex border p-2 text-left cursor-pointer items-top min-w-10 overflow-hidden whitespace-nowrap`}>
+        <div className={`flex border p-2 text-left cursor-pointer items-top min-w-10 overflow-hidden whitespace-nowrap`}
+        onClick={() => setSelectedChat(idChat)}>
             <div className="w-full flex flex-col justify-evenly">
                 <h1 className="w-[90%] truncate">{nomeDoTime}</h1>
                 <p className="truncate">{description}</p>
