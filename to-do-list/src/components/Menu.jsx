b890ea5ed9d96e335e8ee9cfa25bd080f39c1bbe
Levirroh <johann.gr14@gmail.com>
2025-06-15 22:52:17 -0300

    fix: menu spacements

--- to-do-list/src/components/Menu.jsx
+++ to-do-list/src/components/Menu.jsx
@@ -7,16 +7,17 @@ function Menu({ onClose, isOpen }) {
         className={`fixed top-0 left-0 w-64 h-full bg-blue-400 shadow-lg shadow-black p-4 z-50 transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
       >
         <button onClick={onClose} className="mb-4 cursor-pointer">Fechar</button>
-        <div className="h-11/12">
+        <div className="h-11/12 flex flex-col justify-between">
           <div>
             <Li text="Dashboard" link={"/dashboard"} />
             <Li text="Teams" link={"/teams"} />
             <Li text="Configurations" link={"/config"} />
             <Li text="Tasks" link={"/menu"} />
             <Li text="Notifications" link={"/notications"} />
+            <Li text="Chats" link={"/chats"} />
           </div>
           <div>
-            <Li text="Logout" link={"/"} />
+            <Li text="Logout" link={"/"}/>
             </div>
         </div>
       </div>
