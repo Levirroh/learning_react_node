5c180d4121dac3bc963ffead342070cae0a4f848
Levirroh <johann.gr14@gmail.com>
2025-06-18 23:57:06 -0300

    feat: message  component on chat

--- to-do-list/src/components/Chat.jsx
+++ to-do-list/src/components/Chat.jsx
@@ -1,15 +1,13 @@
 import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
+import Message from "./Message.jsx";
 
-function Chat({messagesChat}) {
+function Chat({messagesChat, user}) {
     if (messagesChat != null){
         return (
             <div className="w-full">
                 {messagesChat.map((message) => (
-                     <div key={message.id_message}>
-                        <h1 className="font-bold">{message.content_message}</h1>
-                        <p></p>
-                    </div>
+                     <Message key={message.id_message} message={message} user={user}></Message>
                 ))}
             </div> 
         );
