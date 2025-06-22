00cacc04ddb39d78562790f38020fb33a9f2996f
Levirroh <johann.gr14@gmail.com>
2025-06-21 21:00:47 -0300

    style: chatIcon

--- to-do-list/src/components/ChatIcon.jsx
+++ to-do-list/src/components/ChatIcon.jsx
@@ -3,7 +3,7 @@ import config_icon from "../images/config_icon.svg";
 
 function ChatIcon({idChat, nomeDoTime, description, selectedChat, setSelectedChat}) {
     return (
-        <div className={`flex border p-2 text-left cursor-pointer items-top min-w-10 overflow-hidden whitespace-nowrap`}
+        <div className={`flex border-b p-2 text-left cursor-pointer items-top overflow-hidden whitespace-nowrap`}
         onClick={() => setSelectedChat(idChat)}>
             <div className="w-full flex flex-col justify-evenly">
                 <h1 className="w-[90%] truncate">{nomeDoTime}</h1>
