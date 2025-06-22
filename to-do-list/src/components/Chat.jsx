d60b76071459b9b7f1f818616d42e39523b93598
Levirroh <johann.gr14@gmail.com>
2025-06-21 21:00:40 -0300

    style: chat

--- to-do-list/src/components/Chat.jsx
+++ to-do-list/src/components/Chat.jsx
@@ -5,7 +5,7 @@ import Message from "./Message.jsx";
 function Chat({messagesChat, user}) {
     if (messagesChat != null){
         return (
-            <div className="w-full">
+            <div className="w-full h-[90vh] flex flex-col justify-end">
                 {messagesChat.map((message) => (
                      <Message key={message.id_message} message={message} user={user}></Message>
                 ))}
