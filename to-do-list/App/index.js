3ee6cd1c319ecd4b696eddb3bfabcd7a81af5f33
Levirroh <johann.gr14@gmail.com>
2025-06-23 23:09:26 -0300

    feat: socket for automatic check for new messages

--- to-do-list/App/index.js
+++ to-do-list/App/index.js
@@ -1,12 +1,37 @@
 import express from "express";
-import userRoutes from "./routes/routes.js"
+import userRoutes from "./routes/routes.js";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = http.createServer(app); 
 
-app.use(express.json())
+const io = new Server(server, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Usuário conectado via WebSocket");
+
+    socket.on("sendMessage", (data) => {
+        io.emit("receiveMessage", data); 
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Usuário desconectado");
+    });
+});
+
+app.use(express.json());
 app.use(cors());
 
-app.use("/", userRoutes)
+app.use("/", userRoutes);
 
-app.listen(8800);
+const PORT = 8800;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
