1b54305d3be4ff6942895c1b29c7d5897d4a33ca
Levirroh <johann.gr14@gmail.com>
2025-06-29 22:34:21 -0300

    feat: onclick to open chat, closes newchat

--- to-do-list/src/components/ChatIcon.jsx
+++ to-do-list/src/components/ChatIcon.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import config_icon from "../images/config_icon.svg";
 
-function ChatIcon({idChat, nomeDoTime, description, selectedChat, setSelectedChat}) {
+function ChatIcon({idChat, nomeDoTime, description, selectedChat, setSelectedChat, setFormCreateChat, formCreateChat}) {
+    function openChat(){
+        setSelectedChat(idChat);
+        setFormCreateChat(false);
+    }
     return (
         <div className={`flex border-b p-2 text-left cursor-pointer items-top overflow-hidden whitespace-nowrap`}
-        onClick={() => setSelectedChat(idChat)}>
+        onClick={() => openChat()}>
             <div className="w-full flex flex-col justify-evenly">
                 <h1 className="w-[90%] truncate">{nomeDoTime}</h1>
                 <p className="truncate">{description}</p>
