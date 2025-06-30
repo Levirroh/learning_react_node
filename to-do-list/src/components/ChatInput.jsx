1135c56512ceebb6e25660f0175c780f8c6996d5
Levirroh <johann.gr14@gmail.com>
2025-06-29 22:34:33 -0300

    fix: useless code

--- to-do-list/src/components/ChatInput.jsx
+++ to-do-list/src/components/ChatInput.jsx
@@ -3,13 +3,10 @@ import { useNavigate } from "react-router-dom";
 import Message from "./Message.jsx";
 
 
-function ChatInput({ selectedChat, id_user, newMessage }) {
-
-    return (
-        <div className="w-full h-10 pl-3 pr-3">
-            <input type="text" id="newMessage" className="h-10 bg-gray-300 w-full p-2" placeholder="Digite aqui..." onKeyDown={newMessage}/>
-        </div>
-    );
+function ChatInput({ selectedChat, id_user, newMessage}) {
+    <div className="w-full h-10 pl-3 pr-3">
+        <input type="text" id="newMessage" className="h-10 bg-gray-300 w-full p-2" placeholder="Digite aqui..." onKeyDown={newMessage}/>
+    </div>
 }
 
 export default ChatInput;
