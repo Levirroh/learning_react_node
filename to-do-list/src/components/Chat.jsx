07d623f2b0b9ca3829714fad7773c05fdfbe53af
Levirroh <johann.gr14@gmail.com>
2025-07-05 23:50:31 -0300

    feat: chatConfig popUp

--- to-do-list/src/components/Chat.jsx
+++ to-do-list/src/components/Chat.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import Message from "./Message.jsx";
+import configIcon from "../images/config_icon.svg"
 
-function Chat({messagesChat, formCreateChat, setFormCreateChat, getUserChats}) {
+function Chat({messagesChat, formCreateChat, setFormCreateChat, getUserChats, selectedChat}) {
     const [teams, setTeams] = useState([]);
     const [user, setUser] = useState([]);
     const [newChatName, setNewChatName] = useState("");
@@ -10,6 +11,8 @@ function Chat({messagesChat, formCreateChat, setFormCreateChat, getUserChats}) {
     const [newChatImage, setNewChatImage] = useState("");
     const [newChatDesc, setNewChatDesc] = useState("");
     const [newChatSubject, setNewChatSubject] = useState("");
+    const [popUpChatConfig, setPopUpChatConfig] = useState("");
+    
     const navigate = useNavigate();
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
@@ -71,51 +74,67 @@ function Chat({messagesChat, formCreateChat, setFormCreateChat, getUserChats}) {
                 console.error("Erro ao criar chat:", e);
             }
         } else {
-            console.warn("Preencha todos os campos obrigatórios.");
+            alert("Preencha todos os campos obrigatórios.");
         }
     }
 
-
+    function openChatConfig(){
+        setPopUpChatConfig(!popUpChatConfig)
+    }
 
     if (!formCreateChat){
         if (messagesChat != null){
             return (
                 <div className="w-full h-[89vh] flex flex-col justify-end">
+                    <div className="fixed top-13 right-3">
+                        {selectedChat && (
+                            <div className="w-full flex justify-end p-2">
+                                <img src={configIcon} alt="Configurações" className="h-8 cursor-pointer" onClick={openChatConfig}/>
+                            </div>
+                        )}
+                    </div>
                     {messagesChat.map((message) => (
                          <Message key={message.id_message} message={message} user={user}></Message>
                     ))}
+                    {popUpChatConfig && (
+                        <div className="absolute h-[89vh] w-full flex justify-center items-center">
+                            <div>
+                                <p>Pop Up config</p>
+                            </div>
+                        </div>    
+                    )}
                 </div> 
             );
         }
     } else {
         return(
-            <div className="w-full h-[89vh] flex flex-col justify-end">
-                <form className="flex flex-col gap-10" onSubmit={createChat}>
-                    <div>
-                        <label htmlFor="chatName">Nome</label>
-                        <input className='bg-white p-2' id="chatName" placeholder='Ex.: Chat do trabalho' onChange={(e) => setNewChatName(e.target.value)} />
+            <div className="w-full h-[89vh] flex flex-col justify-center items-center">
+                <form className="flex flex-col gap-10 bg-slate-300 p-20 rounded-2xl" onSubmit={createChat}>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="chatName" className="text-slate-500 font-semibold">Nome:</label>
+                        <input className='bg-white p-2 rounded-2xl' id="chatName" placeholder='Ex.: Chat do trabalho' onChange={(e) => setNewChatName(e.target.value)} />
                     </div>
-                    <div>
-                        <label htmlFor="chatTeam">Time do chat</label>
-                        <select id="chatTeam" className='bg-white p-2' onChange={(e) => setNewChatTeam(e.target.value)}>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="chatTeam" className="text-slate-500 font-semibold">Time do chat</label>
+                        <select id="chatTeam" className='bg-white p-2 rounded-2xl' onChange={(e) => setNewChatTeam(e.target.value)}>
                             {teams.map((team) => (
                                 <option key={team.id_team} value={team.id_team}>{team.name_team}</option>
                             ))}
                         </select>
                     </div>
-                    <div>
-                        <label htmlFor="chatDesc">Descrição</label>
-                        <input className='bg-white p-2' id="chatDesc" placeholder='Ex.: Chat para trabalho' onChange={(e) => setNewChatDesc(e.target.value)} />
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="chatDesc" className="text-slate-500 font-semibold">Descrição</label>
+                        <input className='bg-white p-2 rounded-2xl' id="chatDesc" placeholder='Ex.: Chat para trabalho' onChange={(e) => setNewChatDesc(e.target.value)} />
                     </div>
-                    <div>
-                        <label htmlFor="chatImage">Imagem</label>
-                        <input className='bg-white p-2' id="chatImage" type="image" onChange={(e) => setNewChatImage(e.target.value)} />
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="chatImage" className="text-slate-500 font-semibold">Imagem</label>
+                        <input className='bg-white p-2 rounded-2xl' id="chatImage" type="image" onChange={(e) => setNewChatImage(e.target.value)} />
                     </div>
-                    <div>
-                        <label htmlFor="chatSubject">Assunto</label>
-                        <input className='bg-white p-2' id="chatSubject" placeholder='Ex.: Trabalho' onChange={(e) => setNewChatSubject(e.target.value)} />
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="chatSubject" className="text-slate-500 font-semibold">Assunto</label>
+                        <input className='bg-white p-2 rounded-2xl' id="chatSubject" placeholder='Ex.: Trabalho' onChange={(e) => setNewChatSubject(e.target.value)} />
                     </div>
-                    <button type="submit">Criar chat</button>
+                    <button type="submit" className="bg-white font-semibold rounded-2xl text-slate-600 w-50 h-10 hover:bg-slate-600 hover:text-white transition cursor-pointer">Criar chat</button>
                 </form>
             </div> 
         )
