950bd4080b68c2293aaae5f4d1e4aea4cb716c4d
Levirroh <johann.gr14@gmail.com>
2025-07-05 23:50:39 -0300

    fix: return on chatInput

--- to-do-list/src/components/ChatInput.jsx
+++ to-do-list/src/components/ChatInput.jsx
@@ -3,10 +3,12 @@ import { useNavigate } from "react-router-dom";
 import Message from "./Message.jsx";
 
 
-function ChatInput({ selectedChat, id_user, newMessage}) {
-    <div className="w-full h-10 pl-3 pr-3">
-        <input type="text" id="newMessage" className="h-10 bg-gray-300 w-full p-2" placeholder="Digite aqui..." onKeyDown={newMessage}/>
-    </div>
+function ChatInput({newMessage}) {
+    return(
+        <div className="w-full h-10 pl-3 pr-3">
+            <input type="text" id="newMessage" className="h-10 bg-gray-300 w-full p-2" placeholder="Digite aqui..." onKeyDown={newMessage}/>
+        </div>
+    )
 }
 
 export default ChatInput;
