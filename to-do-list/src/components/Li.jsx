ccb7298aa3690c2f16f2ec29f7ddbac441d39e13
Levirroh <johann.gr14@gmail.com>
2025-07-06 23:34:25 -0300

    feat: new Li usage

--- to-do-list/src/components/Li.jsx
+++ to-do-list/src/components/Li.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-function Li({ link, text }) {
+function Li({ link, text, onclick }) {
+    if (onclick){
+        return (
+            <li className="border border-blue-600 bg-blue-500 rounded-2xl
+            h-12 flex items-center text-white p-3 mt-3 hover:cursor-pointer" onClick={onclick}>{text}</li>
+        );    
+    }
     return (
         <a href={`${link}`} ><li className="border border-blue-600 bg-blue-500 rounded-2xl
         h-12 flex items-center text-white p-3 mt-3 hover:cursor-pointer">{text}</li></a>
