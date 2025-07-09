6fb4bb86fee1a4f3f8c38e64383471d1f36a235b
Levirroh <johann.gr14@gmail.com>
2025-07-08 22:39:29 -0300

    fix: onclick to onClick

--- to-do-list/src/components/Li.jsx
+++ to-do-list/src/components/Li.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Li({ link, text, onclick }) {
-    if (onclick){
+function Li({ link, text, onClick }) {
+    if (onClick){
         return (
             <li className="border border-blue-600 bg-blue-500 rounded-2xl
-            h-12 flex items-center text-white p-3 mt-3 hover:cursor-pointer" onClick={onclick}>{text}</li>
+            h-12 flex items-center text-white p-3 mt-3 hover:cursor-pointer" onClick={onClick}>{text}</li>
         );    
     }
     return (
