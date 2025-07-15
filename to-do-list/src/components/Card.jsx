e9bb19a3bd55e6c78c83fb24e4dca753098add5b
Levirroh <johann.gr14@gmail.com>
2025-07-14 21:36:15 -0300

    fix: link full width

--- to-do-list/src/components/Card.jsx
+++ to-do-list/src/components/Card.jsx
@@ -5,19 +5,13 @@ function Card({ to, text, onClick, color, borderColor, textColor, data1, data2, 
     const bgClass = color ? color : "bg-blue-500";
     const txtColor = textColor ? textColor : "text-white";
 
-    const liContent = (
-        <li className={`border ${bgClass} ${borderClass} rounded-2xl h-15 flex max-w-50 items-center p-3 mt-3 hover:cursor-pointer ${txtColor} bg-gradient-to-br ${gradientFrom} ${gradientTo}`}
-            onClick={onClick}
+    return (
+        <a className={`border ${bgClass} ${borderClass} rounded-2xl h-15 flex max-w-50 items-center p-3 mt-3 hover:cursor-pointer ${txtColor} bg-gradient-to-br ${gradientFrom} ${gradientTo}`}
+            onClick={onClick} href={to}
             >
             {text}
-        </li>
+        </a>
     );
-
-    if (onClick) {
-        return liContent;
-    }
-
-    return <a href={to}>{liContent}</a>;
 }
 
 export default Card;
