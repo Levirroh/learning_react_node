2e734412cec4561750ce43337b3c5a900ffccd46
Levirroh <johann.gr14@gmail.com>
2025-07-14 21:35:38 -0300

    feat: new all data function

--- to-do-list/App/controllers/teams.js
+++ to-do-list/App/controllers/teams.js
@@ -178,3 +178,19 @@ export const updateTeam = (req, res) => {
         return res.status(200).json({ message: "Time atualizado com sucesso!" });
     });
 };
+
+export const get_all_teams_data = (req, res) => {
+    const { id_user } = req.body;
+
+    // const query = "SELECT * FROM chat JOIN team_members ON team_members.team_id = chat.id_team WHERE team_members.user_id = ?;";
+    // const values = [id_user];
+
+    // con.query(query, values, (err, data) => {
+    //     if (err) {
+    //         console.error("Erro ao buscar chat:", err);
+    //         return res.json(err);
+    //     }
+
+    //     return res.status(200).json(data);
+    // });
+};
