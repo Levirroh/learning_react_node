8d77438521d09c7a61a96495ad02e48089eead46
Levirroh <johann.gr14@gmail.com>
2025-07-14 21:35:51 -0300

    feat: route to all new data function

--- to-do-list/App/routes/routes.js
+++ to-do-list/App/routes/routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { login_user} from "../controllers/user.js";
-import { getUserTeams, getStatusTeam, getTeamTasks, new_team_task, createTeam, getTeamMembers, getTeamData, updateTeam} from "../controllers/teams.js";
-import {get_tasks_user, new_task, update_task, get_task_by_id, delete_task, change_status} from "../controllers/tasks.js";
-import {create_new_chat, get_chat_messages, get_chat_users, get_chats_user, new_chat_message, update_chat_users} from "../controllers/chats.js";
+import { getUserTeams, getStatusTeam, getTeamTasks, new_team_task, createTeam, getTeamMembers, getTeamData, updateTeam, get_all_teams_data} from "../controllers/teams.js";
+import {get_tasks_user, new_task, update_task, get_task_by_id, delete_task, change_status, get_all_tasks_data} from "../controllers/tasks.js";
+import {create_new_chat, get_all_chats_data, get_chat_messages, get_chat_users, get_chats_user, new_chat_message, update_chat_users} from "../controllers/chats.js";
 
 
 const router = express.Router();
@@ -14,6 +14,7 @@ router.post("/changeStatus", change_status)
 router.post("/newTask", new_task)
 router.get("/getTaskById/:id", get_task_by_id)
 router.delete("/delete/:id", delete_task)
+router.post("/GetAllTasksData", get_all_tasks_data);
 
 //team taks
 router.post("/newTeamTask", new_team_task);
@@ -29,6 +30,7 @@ router.post("/createTeam", createTeam);
 router.post("/getTeamMembers", getTeamMembers)
 router.post("/updateTeam", updateTeam)
 router.post("/getTeamData", getTeamData);
+router.post("/GetAllTeamsData", get_all_teams_data);
 
 //chat related
 
@@ -38,6 +40,7 @@ router.post("/newMessage", new_chat_message);
 router.post("/createNewChat", create_new_chat);
 router.post("/getChatUsers", get_chat_users);
 router.post("/UpdateChatUsers", update_chat_users);
+router.post("/GetAllChatData", get_all_chats_data);
 
 
 
