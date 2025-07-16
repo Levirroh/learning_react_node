c2b607cc6f554241a4ba74a1f19677ef2bc8b629
Levirroh <johann.gr14@gmail.com>
2025-07-15 21:52:44 -0300

    feat: new data's on card.jsx

--- to-do-list/src/components/Card.jsx
+++ to-do-list/src/components/Card.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { data } from "react-router-dom";
 
 function Card({ to, text, onClick, color, borderColor, textColor, data1, data2, data3, gradientFrom, gradientTo }) {
     const borderClass = borderColor ? borderColor : "border-blue-600";
     const bgClass = color ? color : "bg-blue-500";
     const txtColor = textColor ? textColor : "text-white";
-
+    if (data1 == null){
+        data1 = "?";
+    }
+    if (data2 == null){
+        data2 = "?";
+    }
+    if (data2 == null){
+        data2 = "?";
+    }
     return (
-        <a className={`border ${bgClass} ${borderClass} rounded-2xl h-15 flex max-w-50 items-center p-3 mt-3 hover:cursor-pointer ${txtColor} bg-gradient-to-br ${gradientFrom} ${gradientTo}`}
+        <a className={`border ${bgClass} ${borderClass} rounded-2xl h-25 flex flex-col w-70 p-3 mt-3 hover:cursor-pointer ${txtColor} bg-gradient-to-br ${gradientFrom} ${gradientTo}`}
             onClick={onClick} href={to}
             >
-            {text}
+                <div>
+                    <p className="font-semibold text-2xl">{text}</p>
+                </div>
+                <div>
+                    <p>{data1} / {data2} / {data3}</p>
+                </div>
         </a>
     );
 }
