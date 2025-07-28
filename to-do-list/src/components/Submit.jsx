24f29ba5316167dea8ca6856657fa211629fa355
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-07-28 20:36:19 -0300

    style: transtition from bg-color

--- to-do-list/src/components/Submit.jsx
+++ to-do-list/src/components/Submit.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Submit({text}) {
     return(
-        <button className="bg-gray-200 w-30 p-1 border-1 rounded-2xl shadow-2xl shadow-black cursor-pointer hover:bg-blue-700 hover:text-white hover:border-black transition-all" type="submit">
+        <button className="bg-gray-200 w-30 p-1 border-1 rounded-2xl shadow-2xl shadow-black cursor-pointer hover:bg-blue-400 hover:text-white hover:border-black transition-all duration-400" type="submit">
             {text}
         </button>
     );
