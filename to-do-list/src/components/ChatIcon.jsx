60808a5ba901b963ff266d5e36d74ac54139f95f
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-07-29 19:12:27 -0300

    style: reestyle of chat

--- to-do-list/src/components/ChatIcon.jsx
+++ to-do-list/src/components/ChatIcon.jsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from "react";
 import config_icon from "../images/config_icon.svg";
+function ChatIcon({
+  idChat,
+  nomeDoTime,
+  description,
+  selectedChat,
+  setSelectedChat,
+  setFormCreateChat,
+}) {
+  function openChat() {
+    setSelectedChat(idChat);
+    setFormCreateChat(false);
+  }
 
-function ChatIcon({idChat, nomeDoTime, description, selectedChat, setSelectedChat, setFormCreateChat, formCreateChat}) {
-    function openChat(){
-        setSelectedChat(idChat);
-        setFormCreateChat(false);
-    }
-    return (
-        <div className={`flex border-b p-2 text-left cursor-pointer items-top overflow-hidden whitespace-nowrap`}
-        onClick={() => openChat()}>
-            <div className="w-full flex flex-col justify-evenly">
-                <h1 className="w-[90%] truncate">{nomeDoTime}</h1>
-                <p className="truncate">{description}</p>
-            </div>
-        </div> 
-    );
-    
+  const isSelected = selectedChat === idChat;
+
+  return (
+    <div
+      onClick={openChat}
+      className={`flex flex-col px-4 py-3 cursor-pointer transition 
+        ${isSelected ? "bg-blue-100 border-l-4 border-blue-500" : "hover:bg-blue-50"}`}
+    >
+      <h1 className="text-sm font-semibold text-blue-900 truncate">
+        {nomeDoTime}
+      </h1>
+      <p className="text-xs text-gray-600 truncate">{description}</p>
+    </div>
+  );
 }
-
 export default ChatIcon;
