caad9d2a18ff16c08029db2cc7104f44a82719a9
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-08-09 23:53:35 -0300

    feat: config component

---
+++ to-do-list/src/components/Config.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Config({ text }) {
+        <li className={`border rounded-2xl h-12 flex items-center p-3 mt-3 hover:cursor-pointer`}>
+            Config
+        </li>
+}
+
+export default Config;
