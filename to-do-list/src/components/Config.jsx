b3b12f36e62a683fd7bf3e262f00ca85a5481584
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-09-13 23:48:49 -0300

    feat: added more props

--- to-do-list/src/components/Config.jsx
+++ to-do-list/src/components/Config.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function Config({ name_setting }) {
-        <li className={`border rounded-2xl h-12 flex items-center p-3 mt-3 hover:cursor-pointer`}>
-            {name_setting}
-        </li>
+function Config({ name_setting, type_setting, options_setting = null}) {
+        <div>
+            <li className={`border rounded-2xl h-12 flex items-center p-3 mt-3 hover:cursor-pointer`}>
+                {name_setting}
+            </li>
+            <li className={`border rounded-2xl h-12 flex items-center p-3 mt-3 hover:cursor-pointer`}>
+                {type_setting}
+            </li>
+        </div>
 }
 
 export default Config;
