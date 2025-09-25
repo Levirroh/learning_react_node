4780cbd18414b925460e946985efaabcfaa6ad05
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-09-24 22:06:23 -0300

    style

--- to-do-list/src/components/Config.jsx
+++ to-do-list/src/components/Config.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Config({ name_setting, value_setting, options_setting = null}) {
     return(
-        <div className="border rounded-2xl h-12 flex items-center p-2 mt-3">
+        <div className="border border-blue-600 bg-blue-500 text-white rounded-2xl h-12 flex items-center p-2 mt-3">
             <p>
                 {name_setting}
             </p>
