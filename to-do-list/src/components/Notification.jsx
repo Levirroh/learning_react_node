90b2ff95fc2378950de9156aa8f2fe5f0cecb7e2
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-09-24 22:06:27 -0300

    style

--- to-do-list/src/components/Notification.jsx
+++ to-do-list/src/components/Notification.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Notification({ text, from }) {
     return (
-        <li className={`border  rounded-2xl h-12 flex items-center p-3 mt-3 hover:cursor-pointer`} >
+        <li className={`border border-blue-600 bg-blue-500 text-white rounded-2xl h-12 flex items-center p-2 mt-3`} >
             <a href={`${from}`}>
                 {text}
             </a>
