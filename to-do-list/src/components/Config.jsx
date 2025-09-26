74ac1954b59a9418813d1df2bf81c7b105a47146
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-09-25 22:45:23 -0300

    feat: basic settings

--- to-do-list/src/components/Config.jsx
+++ to-do-list/src/components/Config.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { transformWithEsbuild } from "vite";
 
 function Config({ name_setting, value_setting, options_setting = null}) {
     return(
-        <div className="border border-blue-600 bg-blue-500 text-white rounded-2xl h-12 flex items-center p-2 mt-3">
+        <div className="border border-blue-600 bg-blue-500 text-white rounded-2xl h-12 flex justify-between p-2 mt-3">
             <p>
                 {name_setting}
             </p>
-            <p>
-                {value_setting}
-            </p>
+            {typeof(value_setting) == Boolean ? (
+                <div>
+                    <input type="radio" name={name_setting} value={value_setting} />
+                    <input type="radio" name={name_setting} value={!value_setting} />
+                </div>
+            ) : (
+                <select>
+                    {options_setting.map((option) => (
+                      <option value={option}>{option}</option>      
+                    ))}
+                </select>
+            )}
         </div>
     );
 }
