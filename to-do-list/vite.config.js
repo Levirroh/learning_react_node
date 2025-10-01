12260b1428522e9d2977d6a597814bfbef4af89d
Johann Gossen Ruth <johann.gr14@gmail.com>
2025-09-30 22:08:49 -0300

    new fixes

--- to-do-list/vite.config.js
+++ to-do-list/vite.config.js
@@ -1,16 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['tailwindcss'],
   },
-  plugins: [
-    tailwindcss(),
-  ],
-}
+});
